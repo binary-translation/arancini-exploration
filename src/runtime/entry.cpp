4556d2ee94450e129091f9c24831c161974c9843
Tom Spink <tspink@gmail.com>
2022-09-28 15:32:31 +0100

    Add in some useful signal handling for segfaults.

--- src/runtime/entry.cpp
+++ src/runtime/entry.cpp
@@ -3,6 +3,9 @@
 #include <arancini/runtime/exec/x86/x86-cpu-state.h>
 #include <iostream>
 
+#include <signal.h>
+#include <unistd.h>
+
 #if defined(ARCH_X86_64)
 #include <arancini/output/x86/x86-output-engine.h>
 #elif defined(ARCH_ARM64)
@@ -27,12 +30,42 @@ static arancini::output::arm64::arm64_output_engine oe;
 #error "Unsupported output architecture"
 #endif
 
+static void segv_handler(int signo, siginfo_t *info, void *context)
+{
+	std::cerr << "SEGMENTATION FAULT: code=" << std::hex << info->si_code << ", host-virtual-address=" << std::hex << info->si_addr;
+
+	uintptr_t emulated_base = (uintptr_t)ctx->get_memory_ptr(0);
+	if ((uintptr_t)info->si_addr >= emulated_base) {
+		std::cerr << ", guest-virtual-address=" << std::hex << (info->si_addr - emulated_base);
+	}
+
+	std::cerr << std::endl;
+
+	exit(1);
+}
+
+/*
+ * Initialises signal handling
+ */
+static void init_signals()
+{
+	struct sigaction sa = { 0 };
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &segv_handler;
+	if (sigaction(SIGSEGV, &sa, nullptr) < 0) {
+		throw std::runtime_error("unable to initialise signal handling");
+	}
+}
+
 /*
  * Initialises the dynamic runtime for the guest program that is about to be executed.
  */
 extern "C" void *initialise_dynamic_runtime(unsigned long entry_point)
 {
 	std::cerr << "arancini: dbt: initialise" << std::endl;
+
+	init_signals();
 
 	// Create an execution context for the given input (guest) and output (host) architecture.
 	ctx = new execution_context(ia, oe);
