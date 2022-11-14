7bad027d59e3779216d6f8d937f2cbf225cca73d
Tom Spink <tspink@gmail.com>
2022-11-14 09:51:16 +0000

    Migrate the IR builder to use the create_and_insert<T> helper

--- inc/arancini/ir/ir-builder.h
+++ inc/arancini/ir/ir-builder.h
@@ -19,158 +19,158 @@ public:
 	/// @param vt The type of the constant.
 	/// @param cv The integer value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_i(const value_type &vt, unsigned long cv) { return insert(new constant_node(vt, cv)); }
+	value_node *insert_constant_i(const value_type &vt, unsigned long cv) { return create_and_insert<constant_node>(vt, cv); }
 
 	/// @brief Returns a node representing an floating-point constant.  Works for constants up to a "double" in length.
 	/// @param vt The type of the constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_f(const value_type &vt, double cv) { return insert(new constant_node(vt, cv)); }
+	value_node *insert_constant_f(const value_type &vt, double cv) { return create_and_insert<constant_node>(vt, cv); }
 
 	/// @brief Returns a node representing an 8-bit unsigned integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_u8(unsigned char cv) { return insert(new constant_node(value_type::u8(), (unsigned long)cv)); }
+	value_node *insert_constant_u8(unsigned char cv) { return create_and_insert<constant_node>(value_type::u8(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 16-bit unsigned integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_u16(unsigned short cv) { return insert(new constant_node(value_type::u16(), (unsigned long)cv)); }
+	value_node *insert_constant_u16(unsigned short cv) { return create_and_insert<constant_node>(value_type::u16(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 32-bit unsigned integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_u32(unsigned int cv) { return insert(new constant_node(value_type::u32(), (unsigned long)cv)); }
+	value_node *insert_constant_u32(unsigned int cv) { return create_and_insert<constant_node>(value_type::u32(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 32-bit signed integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_s32(signed int cv) { return insert(new constant_node(value_type::s32(), (unsigned long)cv)); }
+	value_node *insert_constant_s32(signed int cv) { return create_and_insert<constant_node>(value_type::s32(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 64-bit unsigned integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_u64(unsigned long cv) { return insert(new constant_node(value_type::u64(), (unsigned long)cv)); }
+	value_node *insert_constant_u64(unsigned long cv) { return create_and_insert<constant_node>(value_type::u64(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 64-bit signed integer constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_s64(unsigned long cv) { return insert(new constant_node(value_type::s64(), (unsigned long)cv)); }
+	value_node *insert_constant_s64(unsigned long cv) { return create_and_insert<constant_node>(value_type::s64(), (unsigned long)cv); }
 
 	/// @brief Returns a node representing an 32-bit floating point constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_f32(float cv) { return insert(new constant_node(value_type::f32(), (double)cv)); }
+	value_node *insert_constant_f32(float cv) { return create_and_insert<constant_node>(value_type::f32(), (double)cv); }
 
 	/// @brief Returns a node representing an 64-bit floating point constant.
 	/// @param cv The value of the constant.
 	/// @return A constant node.
-	value_node *insert_constant_f64(double cv) { return insert(new constant_node(value_type::f64(), (double)cv)); }
+	value_node *insert_constant_f64(double cv) { return create_and_insert<constant_node>(value_type::f64(), (double)cv); }
 
 	/// @brief Returns a node representing a label used for internal control-flow puproses.
 	/// @return A label node.
-	label_node *insert_label() { return insert(new label_node()); }
+	label_node *insert_label() { return create_and_insert<label_node>(); }
 
 	/// @brief Returns a node representing a conditional branch.  The branch will be taken if the condition evaluates to true.
 	/// @param cond The condition on which to branch.  Non-zero means to take the branch, zero means to NOT take the branch.
 	/// @param target The label node that is the target of the branch.
 	/// @return An action node.
-	action_node *insert_cond_br(port &cond, label_node *target) { return insert(new cond_br_node(cond, target)); }
+	action_node *insert_cond_br(port &cond, label_node *target) { return create_and_insert<cond_br_node>(cond, target); }
 
 	/// @brief Returns a node representing a read of the emulated program counter.
 	/// @return A read pc node.
-	value_node *insert_read_pc() { return insert(new read_pc_node()); }
+	value_node *insert_read_pc() { return create_and_insert<read_pc_node>(); }
 
 	/// @brief Returns a node representing a write of the emulated program counter.
 	/// @param value The new value of the emulated PC.
 	/// @return A write pc node.
-	action_node *insert_write_pc(port &value) { return insert(new write_pc_node(value)); }
+	action_node *insert_write_pc(port &value) { return create_and_insert<write_pc_node>(value); }
 
 	/// @brief read register
 	/// @param vt value type
 	/// @param regoff register offset (use reg_offsets enum values)
 	/// @return a read register node: (vt) regoff
-	value_node *insert_read_reg(const value_type &vt, unsigned long regoff) { return insert(new read_reg_node(vt, regoff)); }
+	value_node *insert_read_reg(const value_type &vt, unsigned long regoff) { return create_and_insert<read_reg_node>(vt, regoff); }
 
 	/// @brief write register
 	/// @param regoff register offset (use reg_offsets enum values)
 	/// @param value value to write
 	/// @return a write register node: regoff := value
-	action_node *insert_write_reg(unsigned long regoff, port &value) { return insert(new write_reg_node(regoff, value)); }
+	action_node *insert_write_reg(unsigned long regoff, port &value) { return create_and_insert<write_reg_node>(regoff, value); }
 
 	/// @brief read memory location
 	/// @param vt value type
 	/// @param addr target address
 	/// @return a read memory node: (vt) [addr]
-	value_node *insert_read_mem(const value_type &vt, port &addr) { return insert(new read_mem_node(vt, addr)); }
+	value_node *insert_read_mem(const value_type &vt, port &addr) { return create_and_insert<read_mem_node>(vt, addr); }
 
 	/// @brief write memory location
 	/// @param addr target address
 	/// @param value value to write
 	/// @return a write memory node: [addr] := value
-	action_node *insert_write_mem(port &addr, port &value) { return insert(new write_mem_node(addr, value)); }
+	action_node *insert_write_mem(port &addr, port &value) { return create_and_insert<write_mem_node>(addr, value); }
 
 	/// @brief addition
 	/// @param lhs first operand
 	/// @param rhs second operand
 	/// @return an add node: lhs + rhs
-	value_node *insert_add(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::add, lhs, rhs)); }
+	value_node *insert_add(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::add, lhs, rhs); }
 
-	value_node *insert_adc(port &lhs, port &rhs, port &top) { return insert(new ternary_arith_node(ternary_arith_op::adc, lhs, rhs, top)); }
+	value_node *insert_adc(port &lhs, port &rhs, port &top) { return create_and_insert<ternary_arith_node>(ternary_arith_op::adc, lhs, rhs, top); }
 
 	/// @brief substraction
 	/// @param lhs first operand
 	/// @param rhs second operand
 	/// @return a sub node: lhs - rhs
-	value_node *insert_sub(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::sub, lhs, rhs)); }
-	value_node *insert_sbb(port &lhs, port &rhs, port &top) { return insert(new ternary_arith_node(ternary_arith_op::sbb, lhs, rhs, top)); }
+	value_node *insert_sub(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::sub, lhs, rhs); }
+	value_node *insert_sbb(port &lhs, port &rhs, port &top) { return create_and_insert<ternary_arith_node>(ternary_arith_op::sbb, lhs, rhs, top); }
 
 	/// @brief multiplication
 	/// @param lhs first operand
 	/// @param rhs second operand
 	/// @return a mul node: lhs * rhs
-	value_node *insert_mul(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::mul, lhs, rhs)); }
+	value_node *insert_mul(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::mul, lhs, rhs); }
 
 	/// @brief division
 	/// @param lhs dividend
 	/// @param rhs divisor
 	/// @return a div node lhs / rhs
-	value_node *insert_div(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::div, lhs, rhs)); }
+	value_node *insert_div(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::div, lhs, rhs); }
 
 	/// @brief bitwise and
 	/// @param lhs first operand
 	/// @param rhs second operand
 	/// @return an and node: lhs & rhs
-	value_node *insert_and(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::band, lhs, rhs)); }
+	value_node *insert_and(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::band, lhs, rhs); }
 
 	/// @brief bitwise or
 	/// @param lhs
 	/// @param rhs
 	/// @return an or node: lhs | rhs
-	value_node *insert_or(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::bor, lhs, rhs)); }
+	value_node *insert_or(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::bor, lhs, rhs); }
 
 	/// @brief bitwise xor
 	/// @param lhs
 	/// @param rhs
 	/// @return an xor node: lhs ^ rhs
-	value_node *insert_xor(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::bxor, lhs, rhs)); }
+	value_node *insert_xor(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::bxor, lhs, rhs); }
 
 	/// @brief bitwise not
 	/// @param lhs
 	/// @return a not node: ~lhs
-	value_node *insert_not(port &lhs) { return insert(new unary_arith_node(unary_arith_op::bnot, lhs)); }
+	value_node *insert_not(port &lhs) { return create_and_insert<unary_arith_node>(unary_arith_op::bnot, lhs); }
 
 	/// @brief compare equal
 	/// @param lhs
 	/// @param rhs
 	/// @return a cmpeq node: lhs == rhs
-	value_node *insert_cmpeq(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::cmpeq, lhs, rhs)); }
+	value_node *insert_cmpeq(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::cmpeq, lhs, rhs); }
 
 	/// @brief compare not equal
 	/// @param lhs
 	/// @param rhs
 	/// @return a cmpne node: lhs != rhs
-	value_node *insert_cmpne(port &lhs, port &rhs) { return insert(new binary_arith_node(binary_arith_op::cmpne, lhs, rhs)); }
+	value_node *insert_cmpne(port &lhs, port &rhs) { return create_and_insert<binary_arith_node>(binary_arith_op::cmpne, lhs, rhs); }
 
 	/// @brief zero extend a value to a larger width (does not preserve the sign)
 	/// @param target_type new type
@@ -182,7 +182,7 @@ public:
 			return value.owner();
 		}
 
-		return insert(new cast_node(cast_op::zx, target_type, value));
+		return create_and_insert<cast_node>(cast_op::zx, target_type, value);
 	}
 
 	/// @brief sign extend a value to a larger width (preserves the sign)
@@ -195,42 +195,42 @@ public:
 			return value.owner();
 		}
 
-		return insert(new cast_node(cast_op::sx, target_type, value));
+		return create_and_insert<cast_node>(cast_op::sx, target_type, value);
 	}
 
 	/// @brief truncate a value to a smaller width
 	/// @param target_type new type
 	/// @param value value to truncate
 	/// @return a trunc node
-	value_node *insert_trunc(const value_type &target_type, port &value) { return insert(new cast_node(cast_op::trunc, target_type, value)); }
+	value_node *insert_trunc(const value_type &target_type, port &value) { return create_and_insert<cast_node>(cast_op::trunc, target_type, value); }
 
-	value_node *insert_bitcast(const value_type &target_type, port &value) { return insert(new cast_node(cast_op::bitcast, target_type, value)); }
+	value_node *insert_bitcast(const value_type &target_type, port &value) { return create_and_insert<cast_node>(cast_op::bitcast, target_type, value); }
 
 	/// @brief Converts a value between type classes, e.g. floating point to integer.  This is NOT a bit cast, it's a value cast.
 	/// @param target_type The type to convert to
 	/// @param value The value being converted
 	/// @return A cast node
-	value_node *insert_convert(const value_type &target_type, port &value) { return insert(new cast_node(cast_op::convert, target_type, value)); }
+	value_node *insert_convert(const value_type &target_type, port &value) { return create_and_insert<cast_node>(cast_op::convert, target_type, value); }
 
-	value_node *insert_csel(port &condition, port &trueval, port &falseval) { return insert(new csel_node(condition, trueval, falseval)); }
+	value_node *insert_csel(port &condition, port &trueval, port &falseval) { return create_and_insert<csel_node>(condition, trueval, falseval); }
 
 	/// @brief logical shift left
 	/// @param input value to shift
 	/// @param amount number of bits to shift
 	/// @return an lsl node: input << amount
-	value_node *insert_lsl(port &input, port &amount) { return insert(new bit_shift_node(shift_op::lsl, input, amount)); }
+	value_node *insert_lsl(port &input, port &amount) { return create_and_insert<bit_shift_node>(shift_op::lsl, input, amount); }
 
 	/// @brief logical shift right, clears the sign
 	/// @param input value to shift
 	/// @param amount number of bits to shift
 	/// @return an lsr node: input >> amount
-	value_node *insert_lsr(port &input, port &amount) { return insert(new bit_shift_node(shift_op::lsr, input, amount)); }
+	value_node *insert_lsr(port &input, port &amount) { return create_and_insert<bit_shift_node>(shift_op::lsr, input, amount); }
 
 	/// @brief arithmetic shift right, preserving the sign
 	/// @param input value to shift
 	/// @param amount number of bits to shift
 	/// @return an asr node: input >> amount
-	value_node *insert_asr(port &input, port &amount) { return insert(new bit_shift_node(shift_op::asr, input, amount)); }
+	value_node *insert_asr(port &input, port &amount) { return create_and_insert<bit_shift_node>(shift_op::asr, input, amount); }
 
 	/// @brief Returns a node representing an extraction of a series of bits from a value.  The input value has bits at position [from:from+length] extracted
 	/// and returned as a value.
@@ -238,7 +238,7 @@ public:
 	/// @param from The (zero based) index of the bit to start extracting from.
 	/// @param length The number of bits to extract.
 	/// @return A bit extract node.
-	value_node *insert_bit_extract(port &input, int from, int length) { return insert(new bit_extract_node(input, from, length)); }
+	value_node *insert_bit_extract(port &input, int from, int length) { return create_and_insert<bit_extract_node>(input, from, length); }
 
 	/// @brief Returns a node representing an insertion of a series of bits into a value.  The input value has the bits inserted at position [to:to+length], and
 	/// returned as a value.
@@ -247,13 +247,13 @@ public:
 	/// @param to The (zero based) index of the bit to start inserting to.
 	/// @param length The number of bits to insert.
 	/// @return A bit insert node.
-	value_node *insert_bit_insert(port &input, port &bits, int to, int length) { return insert(new bit_insert_node(input, bits, to, length)); }
+	value_node *insert_bit_insert(port &input, port &bits, int to, int length) { return create_and_insert<bit_insert_node>(input, bits, to, length); }
 
 	/// @brief Extract an element from a vector.
 	/// @param input The vector to be extracted from.
 	/// @param index The (zero based) index of the element to be extracted.
 	/// @return A vector extract node.
-	value_node *insert_vector_extract(port &input, int index) { return insert(new vector_extract_node(input, index)); };
+	value_node *insert_vector_extract(port &input, int index) { return create_and_insert<vector_extract_node>(input, index); };
 
 	/// @brief Insert an element into a vector.
 	/// @param input The vector to be inserted into.
