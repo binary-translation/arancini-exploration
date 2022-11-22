6dc52f3e149cf8acf88448bcf9ac15789ebe7ba9
Redha Gouicem <redha.gouicem@gmail.com>
2022-11-22 17:25:26 +0100

    add test kernel for punpckhwd

--- test/kernels/sse/unpack.c
+++ test/kernels/sse/unpack.c
@@ -11,3 +11,7 @@ __m128i punpckldq(__m128i m1, __m128i m2) {
 __m128i punpcklwd(__m128i m1, __m128i m2) {
   return _mm_unpacklo_epi16(m1, m2);
 }
+
+__m128i punpckhwd(__m128i m1,__m128i m2) {
+  return _mm_unpackhi_epi16(m1, m2);
+}
