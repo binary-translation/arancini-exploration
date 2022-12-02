72c41147747bc15b51f5127bf56aa270d7fd2d30
Redha Gouicem <redha.gouicem@gmail.com>
2022-12-02 10:10:59 +0100

    support for rep stosq

--- src/input/x86/translators/rep.cpp
+++ src/input/x86/translators/rep.cpp
@@ -39,6 +39,48 @@ void rep_translator::do_translate()
 		break;
 	}
 
+	case XED_ICLASS_REP_STOSQ: {
+		// while rcx != 0; do stosq; rcx--; rdi++; done
+		// stosq - store the content of rax at [rdi]
+		// xed operand encoding: stosq mem base0 rax
+
+		auto cst_0 = builder().insert_constant_u64(0);
+		auto cst_1 = builder().insert_constant_u64(1);
+		auto cst_8 = builder().insert_constant_u64(8);
+
+		auto loop_start = builder().insert_label("while");
+		auto rcx = read_reg(value_type::u64(), reg_offsets::RCX);
+		auto rcx_test = builder().insert_cmpeq(rcx->val(), cst_0->val());
+		cond_br_node *br_loop = (cond_br_node *)builder().insert_cond_br(rcx_test->val(), nullptr);
+
+		auto rax = read_operand(2);
+		auto rdi = read_reg(value_type::u64(), reg_offsets::RDI);
+		builder().insert_write_mem(rdi->val(), rax->val());
+
+		auto df = read_reg(value_type::u1(), reg_offsets::DF);
+		auto df_test = builder().insert_cmpne(df->val(), builder().insert_constant_i(value_type::u1(), 0)->val());
+		cond_br_node *br_df = (cond_br_node *)builder().insert_cond_br(df_test->val(), nullptr);
+
+		write_reg(reg_offsets::RDI, builder().insert_add(rdi->val(), cst_8->val())->val());
+		br_node *br_then = (br_node *)builder().insert_br(nullptr);
+		
+		auto else_label = builder().insert_label("else");
+		br_df->add_br_target(else_label);
+
+		write_reg(reg_offsets::RDI, builder().insert_sub(rdi->val(), cst_8->val())->val());
+
+		auto endif_label = builder().insert_label("endif");
+		br_then->add_br_target(endif_label);
+
+		write_reg(reg_offsets::RCX, builder().insert_sub(rcx->val(), cst_1->val())->val());
+		builder().insert_br(loop_start);
+
+		auto loop_end = builder().insert_label("end");
+		br_loop->add_br_target(loop_end);
+
+		break;
+	}
+
 	default:
 		throw std::runtime_error("unsupported rep instruction");
 	}
