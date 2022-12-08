59c1df8c34b10cede29c23d9789d5cbaa6ca8481
Redha Gouicem <redha.gouicem@gmail.com>
2022-12-08 13:00:33 +0100

    support for neg

--- src/input/x86/translators/unop.cpp
+++ src/input/x86/translators/unop.cpp
@@ -16,6 +16,18 @@ void unop_translator::do_translate()
 		rslt = builder().insert_not(op0->val());
 		break;
 
+	case XED_ICLASS_NEG: {
+		// neg writes the negation of the value in the same register
+		// also sets CF to 1 if the value is not 0, and to 0 if value is 0
+		// Here, we assume that the comparison's value is 1 if true, 0 if false
+		auto zero = builder().insert_constant_i(op0->val().type(), 0);
+		auto cmpz = builder().insert_cmpne(op0->val(), zero->val());
+		cmpz = builder().insert_trunc(value_type::u1(), cmpz->val());
+		rslt = builder().insert_sub(zero->val(), op0->val());
+		write_reg(reg_offsets::CF, cmpz->val());
+		break;
+	}
+
 	default:
 		throw std::runtime_error("unsupported unop");
 	}
@@ -25,6 +37,9 @@ void unop_translator::do_translate()
 	switch (xed_decoded_inst_get_iclass(xed_inst())) {
 	case XED_ICLASS_NOT:
 		write_flags(rslt, flag_op::update, flag_op::set0, flag_op::set0, flag_op::update, flag_op::update, flag_op::ignore);
+		break;
+	case XED_ICLASS_NEG:
+		write_flags(rslt, flag_op::update, flag_op::ignore, flag_op::update, flag_op::update, flag_op::update, flag_op::update);
 		break;
 
 	default:
