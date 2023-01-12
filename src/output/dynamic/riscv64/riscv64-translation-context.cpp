497aa19fd5e12a2166bc70a259e037794f21cd14
Simon Kammermeier <simon.kammermeier@tum.de>
2023-01-12 18:25:49 +0100

    Fixed duplicate switch branch.
    Not sure what happened here

--- src/output/dynamic/riscv64/riscv64-translation-context.cpp
+++ src/output/dynamic/riscv64/riscv64-translation-context.cpp
@@ -196,19 +196,28 @@ Register riscv64_translation_context::materialise(const node *n)
 			case 128:
 				// Split calculation
 				assembler.mul(outReg, srcReg1, srcReg2);
-				if (n2->val().type().element_type().type_class() == ir::value_type_class::signed_integer)
-
-				{
+				if (n2->val().type().element_type().type_class() == ir::value_type_class::signed_integer) {
 					assembler.mulh(outReg2, srcReg1, srcReg2);
-					// TODO Flags
+					assembler.srai(CF, outReg, 64);
+					assembler.xor_(CF, CF, outReg2);
+					assembler.snez(CF, CF);
 				} else {
 					assembler.mulhu(outReg2, srcReg1, srcReg2);
 					assembler.snez(CF, outReg2);
 				}
 				assembler.mv(OF, CF);
-
 				break;
 			case 64:
+				assembler.mul(outReg, srcReg1, srcReg2);
+				if (n2->val().type().element_type().type_class() == ir::value_type_class::signed_integer) {
+					assembler.sraiw(CF, outReg, 32);
+					assembler.srli(CF, CF, 32);
+					assembler.snez(CF, CF);
+				} else {
+
+					// assembler.snez(CF,);
+				}
+				break;
 			case 32:
 			case 16:
 			case 8:
@@ -219,167 +228,7 @@ Register riscv64_translation_context::materialise(const node *n)
 		case binary_arith_op::mod:
 		case binary_arith_op::cmpeq:
 		case binary_arith_op::cmpne:
-			throw std::runtime_error("unsupported binary arithmetic operation");
-		}
-
-		assembler.seqz(ZF, outReg); // ZF
-		assembler.sltz(SF, outReg); // SF
-
-		return outReg;
-	}
-	case node_kinds::binary_arith: {
-		auto n2 = (binary_arith_node *)n;
-		Register outReg = T0;
-
-		Register srcReg1 = materialise(n2->lhs().owner());
-
-		if (n2->rhs().owner()->kind() == node_kinds::constant) {
-			// Could also work for LHS except sub
-			// TODO Probably incorrect to just cast to signed 64bit
-			auto imm = (intptr_t)((constant_node *)(n2->rhs().owner()))->const_val_i();
-			if (imm)
-
-				if (IsITypeImm(imm)) {
-					switch (n2->op()) {
-
-					case binary_arith_op::sub:
-						if (imm == -2048) { // Can happen with inversion
-							goto standardPath;
-						}
-						switch (n2->val().type().width()) {
-						case 64:
-							assembler.addi(outReg, srcReg1, -imm);
-							break;
-						case 32:
-							assembler.addiw(outReg, srcReg1, -imm);
-							break;
-						case 8:
-						case 16:
-							assembler.addi(outReg, srcReg1, -imm);
-							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
-							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
-							break;
-						}
-						assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
-						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
-						if (imm > 0) {
-							assembler.xori(OF, OF, 1); // Invert on positive
-						}
-						break;
-
-					case binary_arith_op::add:
-
-						switch (n2->val().type().width()) {
-						case 64:
-							assembler.addi(outReg, srcReg1, imm);
-							break;
-						case 32:
-							assembler.addiw(outReg, srcReg1, -imm);
-							break;
-						case 8:
-						case 16:
-							assembler.addi(outReg, srcReg1, imm);
-							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
-							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
-							break;
-						}
-
-						assembler.sltu(CF, outReg, srcReg1); // CF FIXME Assumes outReg!=srcReg1
-						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
-						if (imm < 0) {
-							assembler.xori(OF, OF, 1); // Invert on negative
-						}
-
-						break;
-					// Binary operations preserve sign extension
-					case binary_arith_op::band:
-						assembler.andi(outReg, srcReg1, imm);
-						break;
-					case binary_arith_op::bor:
-						assembler.ori(outReg, srcReg1, imm);
-						break;
-					case binary_arith_op::bxor:
-						assembler.xori(outReg, srcReg1, imm);
-						break;
-					default:
-						// No-op Go to standard path
-						goto standardPath;
-					}
-
-					assembler.seqz(ZF, outReg); // ZF
-					assembler.sltz(SF, outReg); // SF
-
-					return outReg;
-				}
-		}
-
-	standardPath:
-		Register srcReg2 = materialise(n2->rhs().owner());
-		switch (n2->op()) {
-
-		case binary_arith_op::add:
-			switch (n2->val().type().width()) {
-			case 64:
-				assembler.add(outReg, srcReg1, srcReg2);
-				break;
-			case 32:
-				assembler.addw(outReg, srcReg1, srcReg2);
-				break;
-			case 8:
-			case 16:
-				assembler.add(outReg, srcReg1, srcReg2);
-				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
-				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
-				break;
-			}
-
-			assembler.sltz(CF, srcReg1);
-			assembler.slt(OF, outReg, srcReg2);
-			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
-
-			assembler.sltu(CF, outReg, srcReg2); // CF (Allows typical x86 case of regSrc1==outReg) FIXME Assumes outReg!=srcReg2
-			break;
-
-		case binary_arith_op::sub:
-			switch (n2->val().type().width()) {
-			case 64:
-				assembler.sub(outReg, srcReg1, srcReg2);
-				break;
-			case 32:
-				assembler.subw(outReg, srcReg1, srcReg2);
-				break;
-			case 8:
-			case 16:
-				assembler.sub(outReg, srcReg1, srcReg2);
-				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
-				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
-				break;
-			}
-			assembler.sub(outReg, srcReg1, srcReg2);
-
-			assembler.sgtz(CF, srcReg2);
-			assembler.slt(OF, outReg, srcReg1);
-			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
-
-			assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
-			break;
-
-		// Binary operations preserve sign extension
-		case binary_arith_op::band:
-			assembler.and_(outReg, srcReg1, srcReg2);
-			break;
-		case binary_arith_op::bor:
-			assembler.or_(outReg, srcReg1, srcReg2);
-			break;
-		case binary_arith_op::bxor:
-			assembler.xor_(outReg, srcReg1, srcReg2);
-			break;
-
-		case binary_arith_op::mul:
-		case binary_arith_op::div:
-		case binary_arith_op::mod:
-		case binary_arith_op::cmpeq:
-		case binary_arith_op::cmpne:
+		case binary_arith_op::cmpgt:
 			throw std::runtime_error("unsupported binary arithmetic operation");
 		}
 
