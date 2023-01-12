eabc4222fc97f7e5082571359a8a66f24f80f2f5
Simon Kammermeier <simon.kammermeier@tum.de>
2023-01-12 15:12:23 +0100

    First part of binary arithmetic
    Does not include cmp nodes, div or rem.
    Mul support partial

--- src/output/dynamic/riscv64/riscv64-translation-context.cpp
+++ src/output/dynamic/riscv64/riscv64-translation-context.cpp
@@ -5,6 +5,16 @@
 using namespace arancini::output::dynamic::riscv64;
 using namespace arancini::ir;
 
+/**
+ * All values are always held sign extended to 64 bits in RISCV registers for simple flag calculation
+ * Translations assumes FP holds pointer to CPU state.
+ * Flags are always stored in registers S8 (ZF), S9 (CF), S10 (OF), S11(SF)
+ */
+constexpr Register ZF = S8;
+constexpr Register CF = S9;
+constexpr Register OF = S10;
+constexpr Register SF = S11;
+
 void riscv64_translation_context::begin_block() { }
 void riscv64_translation_context::begin_instruction(off_t address, const std::string &disasm) { }
 void riscv64_translation_context::end_instruction() { }
@@ -16,13 +26,367 @@ Register riscv64_translation_context::materialise(const node *n)
 	case node_kinds::write_reg: {
 
 		auto n2 = (write_reg_node *)n;
+
 		port &value = n2->value();
-		Register regVal = materialise(value.owner());
-		assembler.sd(regVal, { FP, static_cast<intptr_t>(n2->regoff()) });
+		if (value.type().width() == 1) { // Flags
+			// TODO
+		} else if (value.type().width() == 64) { // GPR
+
+			Register regVal = materialise(value.owner());
+			assembler.sd(regVal, { FP, static_cast<intptr_t>(n2->regoff()) });
+		} else {
+			throw std::runtime_error("Unsupported width on register write");
+		}
 
 	} break;
 	case node_kinds::constant: {
 		return materialiseConstant((int64_t)((constant_node *)n)->const_val_i());
+	}
+	case node_kinds::binary_arith: {
+		auto n2 = (binary_arith_node *)n;
+		Register outReg = T0;
+		Register outReg2 = T1;
+
+		Register srcReg1 = materialise(n2->lhs().owner());
+
+		if (n2->rhs().owner()->kind() == node_kinds::constant) {
+			// Could also work for LHS except sub
+			// TODO Probably incorrect to just cast to signed 64bit
+			auto imm = (intptr_t)((constant_node *)(n2->rhs().owner()))->const_val_i();
+			if (imm)
+
+				if (IsITypeImm(imm)) {
+					switch (n2->op()) {
+
+					case binary_arith_op::sub:
+						if (imm == -2048) { // Can happen with inversion
+							goto standardPath;
+						}
+						switch (n2->val().type().width()) {
+						case 64:
+							assembler.addi(outReg, srcReg1, -imm);
+							break;
+						case 32:
+							assembler.addiw(outReg, srcReg1, -imm);
+							break;
+						case 8:
+						case 16:
+							assembler.addi(outReg, srcReg1, -imm);
+							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+							break;
+						}
+						assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
+						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
+						if (imm > 0) {
+							assembler.xori(OF, OF, 1); // Invert on positive
+						}
+						break;
+
+					case binary_arith_op::add:
+
+						switch (n2->val().type().width()) {
+						case 64:
+							assembler.addi(outReg, srcReg1, imm);
+							break;
+						case 32:
+							assembler.addiw(outReg, srcReg1, -imm);
+							break;
+						case 8:
+						case 16:
+							assembler.addi(outReg, srcReg1, imm);
+							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+							break;
+						}
+
+						assembler.sltu(CF, outReg, srcReg1); // CF FIXME Assumes outReg!=srcReg1
+						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
+						if (imm < 0) {
+							assembler.xori(OF, OF, 1); // Invert on negative
+						}
+
+						break;
+					// Binary operations preserve sign extension
+					case binary_arith_op::band:
+						assembler.andi(outReg, srcReg1, imm);
+						break;
+					case binary_arith_op::bor:
+						assembler.ori(outReg, srcReg1, imm);
+						break;
+					case binary_arith_op::bxor:
+						assembler.xori(outReg, srcReg1, imm);
+						break;
+					default:
+						// No-op Go to standard path
+						goto standardPath;
+					}
+
+					assembler.seqz(ZF, outReg); // ZF
+					assembler.sltz(SF, outReg); // SF
+
+					return outReg;
+				}
+		}
+
+	standardPath:
+		Register srcReg2 = materialise(n2->rhs().owner());
+		switch (n2->op()) {
+
+		case binary_arith_op::add:
+			switch (n2->val().type().width()) {
+			case 64:
+				assembler.add(outReg, srcReg1, srcReg2);
+				break;
+			case 32:
+				assembler.addw(outReg, srcReg1, srcReg2);
+				break;
+			case 8:
+			case 16:
+				assembler.add(outReg, srcReg1, srcReg2);
+				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+				break;
+			}
+
+			assembler.sltz(CF, srcReg1);
+			assembler.slt(OF, outReg, srcReg2);
+			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
+
+			assembler.sltu(CF, outReg, srcReg2); // CF (Allows typical x86 case of regSrc1==outReg) FIXME Assumes outReg!=srcReg2
+			break;
+
+		case binary_arith_op::sub:
+			switch (n2->val().type().width()) {
+			case 64:
+				assembler.sub(outReg, srcReg1, srcReg2);
+				break;
+			case 32:
+				assembler.subw(outReg, srcReg1, srcReg2);
+				break;
+			case 8:
+			case 16:
+				assembler.sub(outReg, srcReg1, srcReg2);
+				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+				break;
+			}
+			assembler.sub(outReg, srcReg1, srcReg2);
+
+			assembler.sgtz(CF, srcReg2);
+			assembler.slt(OF, outReg, srcReg1);
+			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
+
+			assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
+			break;
+
+		// Binary operations preserve sign extension
+		case binary_arith_op::band:
+			assembler.and_(outReg, srcReg1, srcReg2);
+			break;
+		case binary_arith_op::bor:
+			assembler.or_(outReg, srcReg1, srcReg2);
+			break;
+		case binary_arith_op::bxor:
+			assembler.xor_(outReg, srcReg1, srcReg2);
+			break;
+
+		case binary_arith_op::mul:
+			switch (n2->val().type().width()) {
+			case 128:
+				// Split calculation
+				assembler.mul(outReg, srcReg1, srcReg2);
+				if (n2->val().type().element_type().type_class() == ir::value_type_class::signed_integer)
+
+				{
+					assembler.mulh(outReg2, srcReg1, srcReg2);
+					// TODO Flags
+				} else {
+					assembler.mulhu(outReg2, srcReg1, srcReg2);
+					assembler.snez(CF, outReg2);
+				}
+				assembler.mv(OF, CF);
+
+				break;
+			case 64:
+			case 32:
+			case 16:
+			case 8:
+				// TODO
+				break;
+			}
+		case binary_arith_op::div:
+		case binary_arith_op::mod:
+		case binary_arith_op::cmpeq:
+		case binary_arith_op::cmpne:
+			throw std::runtime_error("unsupported binary arithmetic operation");
+		}
+
+		assembler.seqz(ZF, outReg); // ZF
+		assembler.sltz(SF, outReg); // SF
+
+		return outReg;
+	}
+	case node_kinds::binary_arith: {
+		auto n2 = (binary_arith_node *)n;
+		Register outReg = T0;
+
+		Register srcReg1 = materialise(n2->lhs().owner());
+
+		if (n2->rhs().owner()->kind() == node_kinds::constant) {
+			// Could also work for LHS except sub
+			// TODO Probably incorrect to just cast to signed 64bit
+			auto imm = (intptr_t)((constant_node *)(n2->rhs().owner()))->const_val_i();
+			if (imm)
+
+				if (IsITypeImm(imm)) {
+					switch (n2->op()) {
+
+					case binary_arith_op::sub:
+						if (imm == -2048) { // Can happen with inversion
+							goto standardPath;
+						}
+						switch (n2->val().type().width()) {
+						case 64:
+							assembler.addi(outReg, srcReg1, -imm);
+							break;
+						case 32:
+							assembler.addiw(outReg, srcReg1, -imm);
+							break;
+						case 8:
+						case 16:
+							assembler.addi(outReg, srcReg1, -imm);
+							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+							break;
+						}
+						assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
+						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
+						if (imm > 0) {
+							assembler.xori(OF, OF, 1); // Invert on positive
+						}
+						break;
+
+					case binary_arith_op::add:
+
+						switch (n2->val().type().width()) {
+						case 64:
+							assembler.addi(outReg, srcReg1, imm);
+							break;
+						case 32:
+							assembler.addiw(outReg, srcReg1, -imm);
+							break;
+						case 8:
+						case 16:
+							assembler.addi(outReg, srcReg1, imm);
+							assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+							assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+							break;
+						}
+
+						assembler.sltu(CF, outReg, srcReg1); // CF FIXME Assumes outReg!=srcReg1
+						assembler.slt(OF, outReg, srcReg1); // OF FIXME Assumes outReg!=srcReg1
+						if (imm < 0) {
+							assembler.xori(OF, OF, 1); // Invert on negative
+						}
+
+						break;
+					// Binary operations preserve sign extension
+					case binary_arith_op::band:
+						assembler.andi(outReg, srcReg1, imm);
+						break;
+					case binary_arith_op::bor:
+						assembler.ori(outReg, srcReg1, imm);
+						break;
+					case binary_arith_op::bxor:
+						assembler.xori(outReg, srcReg1, imm);
+						break;
+					default:
+						// No-op Go to standard path
+						goto standardPath;
+					}
+
+					assembler.seqz(ZF, outReg); // ZF
+					assembler.sltz(SF, outReg); // SF
+
+					return outReg;
+				}
+		}
+
+	standardPath:
+		Register srcReg2 = materialise(n2->rhs().owner());
+		switch (n2->op()) {
+
+		case binary_arith_op::add:
+			switch (n2->val().type().width()) {
+			case 64:
+				assembler.add(outReg, srcReg1, srcReg2);
+				break;
+			case 32:
+				assembler.addw(outReg, srcReg1, srcReg2);
+				break;
+			case 8:
+			case 16:
+				assembler.add(outReg, srcReg1, srcReg2);
+				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+				break;
+			}
+
+			assembler.sltz(CF, srcReg1);
+			assembler.slt(OF, outReg, srcReg2);
+			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
+
+			assembler.sltu(CF, outReg, srcReg2); // CF (Allows typical x86 case of regSrc1==outReg) FIXME Assumes outReg!=srcReg2
+			break;
+
+		case binary_arith_op::sub:
+			switch (n2->val().type().width()) {
+			case 64:
+				assembler.sub(outReg, srcReg1, srcReg2);
+				break;
+			case 32:
+				assembler.subw(outReg, srcReg1, srcReg2);
+				break;
+			case 8:
+			case 16:
+				assembler.sub(outReg, srcReg1, srcReg2);
+				assembler.slli(outReg, outReg, 64 - n2->val().type().width());
+				assembler.srai(outReg, outReg, 64 - n2->val().type().width());
+				break;
+			}
+			assembler.sub(outReg, srcReg1, srcReg2);
+
+			assembler.sgtz(CF, srcReg2);
+			assembler.slt(OF, outReg, srcReg1);
+			assembler.xor_(OF, OF, CF); // OF FIXME Assumes outReg!=srcReg1 && outReg!=srcReg2
+
+			assembler.sltu(CF, srcReg1, outReg); // CF FIXME Assumes outReg!=srcReg1
+			break;
+
+		// Binary operations preserve sign extension
+		case binary_arith_op::band:
+			assembler.and_(outReg, srcReg1, srcReg2);
+			break;
+		case binary_arith_op::bor:
+			assembler.or_(outReg, srcReg1, srcReg2);
+			break;
+		case binary_arith_op::bxor:
+			assembler.xor_(outReg, srcReg1, srcReg2);
+			break;
+
+		case binary_arith_op::mul:
+		case binary_arith_op::div:
+		case binary_arith_op::mod:
+		case binary_arith_op::cmpeq:
+		case binary_arith_op::cmpne:
+			throw std::runtime_error("unsupported binary arithmetic operation");
+		}
+
+		assembler.seqz(ZF, outReg); // ZF
+		assembler.sltz(SF, outReg); // SF
+
+		return outReg;
 	}
 	default:
 		throw std::runtime_error("unsupported node");
@@ -31,8 +395,11 @@ Register riscv64_translation_context::materialise(const node *n)
 }
 Register riscv64_translation_context::materialiseConstant(int64_t imm)
 {
-	//Optimizations with left or right shift at the end not implemented (for constants with trailing or leading zeroes)
+	// Optimizations with left or right shift at the end not implemented (for constants with trailing or leading zeroes)
 
+	if (imm == 0) {
+		return ZERO;
+	}
 	Register outReg = A0;
 	auto immLo32 = (int32_t)imm;
 	auto immHi32 = imm >> 32 << 32;
@@ -49,12 +416,13 @@ Register riscv64_translation_context::materialiseConstant(int64_t imm)
 		}
 
 	} else {
-		auto val = (int64_t)((uint64_t)imm - (uint64_t)(int64_t)immLo12); //Get lower 12 bits out of imm
+		auto val = (int64_t)((uint64_t)imm - (uint64_t)(int64_t)immLo12); // Get lower 12 bits out of imm
 		int shiftAmnt = 0;
-		if (!Utils::IsInt(32, val)) { //Might still not fit as LUI with unsigned add
+		if (!Utils::IsInt(32, val)) { // Might still not fit as LUI with unsigned add
 			shiftAmnt = __builtin_ctzll(val);
 			val >>= shiftAmnt;
-			if (shiftAmnt > 12 && !IsITypeImm(val)&& Utils::IsInt(32, val<<12)) { //Does not fit into 12 bits but can fit into LUI U-immediate with proper shift
+			if (shiftAmnt > 12 && !IsITypeImm(val)
+				&& Utils::IsInt(32, val << 12)) { // Does not fit into 12 bits but can fit into LUI U-immediate with proper shift
 				val <<= 12;
 				shiftAmnt -= 12;
 			}
@@ -69,7 +437,6 @@ Register riscv64_translation_context::materialiseConstant(int64_t imm)
 		if (immLo12) {
 			assembler.addi(outReg, outReg, immLo12);
 		}
-
 	}
 	return outReg;
 }
