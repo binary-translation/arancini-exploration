7b49de1cc221ba9e6cfb259d252b0a2f71487d9e
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-01-24 00:20:21 +0100

    Add toolchains for cross-compilation

---
+++ aarch64-toolchain.cmake
@@ -0,0 +1,35 @@
+# Toolchain file for AARCH64 on GNU Linux
+#
+# Use cmake -B <build> --toolchain aarch64-toolchain.cmake
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(TOOLCHAIN_PREFIX aarch64-linux-gnu)
+execute_process(
+  COMMAND which ${TOOLCHAIN_PREFIX}-gcc
+  OUTPUT_VARIABLE BINUTILS_PATH
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+get_filename_component(ARM_TOOLCHAIN_DIR ${BINUTILS_PATH} DIRECTORY)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+
+# Disable compiler checks
+#
+# CMake will try to run various test programs to determine compiler
+# capabilities
+#
+# Those programs can't run when cross-compiling
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+set(CMAKE_SYSROOT ${ARM_TOOLCHAIN_DIR})
+set(CMAKE_FIND_ROOT_PATH ${BINUTILS_PATH})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
