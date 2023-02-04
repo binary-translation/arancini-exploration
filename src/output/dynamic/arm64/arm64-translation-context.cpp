01de873b04a16b8a8797388c0529f4b0285fa91a
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-02-04 22:48:09 +0100

    [AARCH64 DBT] Add the Keystone assembler as a dependency

--- src/output/dynamic/arm64/arm64-translation-context.cpp
+++ src/output/dynamic/arm64/arm64-translation-context.cpp
@@ -6,4 +6,9 @@ void arm64_translation_context::begin_block() { }
 void arm64_translation_context::begin_instruction(off_t address, const std::string &disasm) { }
 void arm64_translation_context::end_instruction() { }
 void arm64_translation_context::end_block() { }
-void arm64_translation_context::lower(const std::shared_ptr<ir::action_node> &n) { }
+void arm64_translation_context::lower(ir::node *n) { }
+
+void arm64_translation_context::materialise(const ir::node* n) {
+
+}
+
