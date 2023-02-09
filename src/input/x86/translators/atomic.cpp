f60ee9d639ec060942d25850a67096428b3f3781
Redha Gouicem <redha.gouicem@gmail.com>
2023-02-09 17:17:04 +0100

    ir: fix atomics semantics
    Atomic operations now have more standard semantics, with the destination operand
    always being a memory address, and the other operands being registers.

--- src/input/x86/translators/atomic.cpp
+++ src/input/x86/translators/atomic.cpp
@@ -20,20 +20,21 @@ void atomic_translator::do_translate()
   }
 
   case XED_ICLASS_XCHG: {
-    auto op0 = read_operand(0);
-    auto op1 = read_operand(1);
-
-    if (is_memory_operand(0) || is_memory_operand(1)) { // atomic
-      builder().insert_atomic_xchg(op0->val(), op1->val());
-    } else { // non-atomic
-      write_operand(0, op1->val());
-      write_operand(1, op0->val());
+    // if an operand is memory, xchg becomes atomic
+    if (is_memory_operand(0)) {
+      builder().insert_atomic_xchg(compute_address(0)->val(), read_operand(1)->val());
+    } else if (is_memory_operand(1)) {
+      builder().insert_atomic_xchg(compute_address(1)->val(), read_operand(0)->val());
+    } else {
+      // TODO fix with a temp register node
+      write_operand(0, read_operand(1)->val());
+      write_operand(1, read_operand(0)->val());
     }
     break;
   }
 
   case XED_ICLASS_CMPXCHG_LOCK: {
-    auto dst = read_operand(0);
+    auto dst = compute_address(0);
     auto acc = read_operand(1);
     auto src = read_operand(2);
 
@@ -43,16 +44,16 @@ void atomic_translator::do_translate()
   }
 
   case XED_ICLASS_ADD_LOCK: {
-    auto op0 = read_operand(0);
-    auto op1 = read_operand(1);
+    auto dst = compute_address(0);
+    auto src = read_operand(1);
 
-    auto res = builder().insert_atomic_binop(binary_atomic_op::add, op0->val(), op1->val());
+    auto res = builder().insert_atomic_binop(binary_atomic_op::add, dst->val(), src->val());
     write_flags(res, flag_op::update, flag_op::update, flag_op::update, flag_op::update, flag_op::update, flag_op::update);
     break;
   }
 
   case XED_ICLASS_AND_LOCK: {
-    auto op0 = read_operand(0);
+    auto op0 = compute_address(0);
     auto op1 = read_operand(1);
 
     auto res = builder().insert_atomic_binop(binary_atomic_op::band, op0->val(), op1->val());
@@ -61,7 +62,7 @@ void atomic_translator::do_translate()
   }
 
   case XED_ICLASS_OR_LOCK: {
-    auto op0 = read_operand(0);
+    auto op0 = compute_address(0);
     auto op1 = read_operand(1);
 
     auto res = builder().insert_atomic_binop(binary_atomic_op::bor, op0->val(), op1->val());
@@ -70,7 +71,7 @@ void atomic_translator::do_translate()
 	}
 
   case XED_ICLASS_DEC_LOCK: {
-    auto src = read_operand(0);
+    auto src = compute_address(0);
     builder().insert_atomic_binop(binary_atomic_op::sub, src->val(), builder().insert_constant_i(src->val().type(), 1)->val());
     break;
   }
