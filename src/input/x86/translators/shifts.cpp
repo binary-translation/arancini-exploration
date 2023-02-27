9260286bc6c97ea1d079246eae6eb3651bde18d0
Redha Gouicem <redha.gouicem@gmail.com>
2023-02-27 16:46:09 +0100

    front: fix shift behaviors (flags are set + handles shift amount in register)

--- src/input/x86/translators/shifts.cpp
+++ src/input/x86/translators/shifts.cpp
@@ -8,20 +8,104 @@ using namespace arancini::input::x86::translators;
 void shifts_translator::do_translate()
 {
 	auto src = read_operand(0);
-	auto amt = builder().insert_constant_u32(xed_decoded_inst_get_unsigned_immediate(xed_inst()));
+  auto amt = read_operand(1);
 
-	switch (xed_decoded_inst_get_iclass(xed_inst())) {
+  auto inst = xed_decoded_inst_get_iclass(xed_inst());
+	switch (inst) {
 	case XED_ICLASS_SAR:
-		write_operand(0, builder().insert_asr(src->val(), amt->val())->val());
-		break;
-
 	case XED_ICLASS_SHR:
-		write_operand(0, builder().insert_lsr(src->val(), amt->val())->val());
-		break;
+	case XED_ICLASS_SHL: {
+    if (is_immediate_operand(1)) { // shift amount is an immediate
+      auto rot_val = ((constant_node *)amt)->const_val_i();
 
-	case XED_ICLASS_SHL:
-		write_operand(0, builder().insert_lsl(src->val(), amt->val())->val());
+      if (rot_val != 0) {
+        value_node *shift;
+        if (inst == XED_ICLASS_SAR) {
+          shift = builder().insert_asr(src->val(), amt->val());
+        } else if (inst == XED_ICLASS_SHR) {
+          shift = builder().insert_lsr(src->val(), amt->val());
+        } else {
+          shift = builder().insert_lsl(src->val(), amt->val());
+        }
+        write_operand(0, shift->val());
+
+        // CF flag receives the last shifted bit
+        value_node *last_shift_bit;
+        if (inst == XED_ICLASS_SHL) {
+          last_shift_bit = builder().insert_bit_extract(src->val(), src->val().type().width() - rot_val, 1);
+        } else { // SHR/SAR
+          last_shift_bit = builder().insert_bit_extract(src->val(), rot_val - 1, 1);
+        }
+        write_reg(reg_offsets::CF, last_shift_bit->val());
+
+        // OF is set if shifting by 1 only
+        if (rot_val == 1) {
+          if (inst == XED_ICLASS_SHL) {
+            // for left shifts, OF is set to 0 iff most significant bit of the result equals CF
+            auto msb = builder().insert_bit_extract(shift->val(), shift->val().type().width() - 1, 1);
+            auto of = builder().insert_xor(last_shift_bit->val(), msb->val());
+            write_reg(reg_offsets::OF, of->val());
+          } else if (inst == XED_ICLASS_SAR) {
+            // for SAR, OF is cleared
+            write_reg(reg_offsets::OF, builder().insert_constant_u8(0)->val());
+          } else {
+            // for SHR, OF is set to most significant bit of original operand
+            auto msb = builder().insert_bit_extract(src->val(), src->val().type().width() - 1, 1);
+            write_reg(reg_offsets::OF, msb->val());
+          }
+        }
+      }
+    } else { // shift amount is a register
+      auto zero_shift = builder().insert_cmpeq(amt->val(), builder().insert_constant_i(amt->val().type(), 0)->val());
+      cond_br_node *zero_br = (cond_br_node *)builder().insert_cond_br(zero_shift->val(), nullptr);
+
+      // shift amount is not 0
+      value_node *shift;
+      if (inst == XED_ICLASS_SAR) {
+        shift = builder().insert_asr(src->val(), amt->val());
+      } else if (inst == XED_ICLASS_SHR) {
+        shift = builder().insert_lsr(src->val(), amt->val());
+      } else {
+        shift = builder().insert_lsl(src->val(), amt->val());
+      }
+      write_operand(0, shift->val());
+
+      // CF flag receives the last shifted bit
+      value_node *last_shift_bit;
+      auto amt_min_1 = builder().insert_sub(amt->val(), builder().insert_constant_i(amt->val().type(), 1)->val());
+      if (inst == XED_ICLASS_SHL) {
+        auto tmp_shift = builder().insert_lsl(src->val(), amt_min_1->val());
+        last_shift_bit = builder().insert_bit_extract(tmp_shift->val(), src->val().type().width() - 1, 1);
+      } else { // SHR/SAR
+        auto tmp_shift = builder().insert_lsr(src->val(), amt_min_1->val());
+        last_shift_bit = builder().insert_bit_extract(tmp_shift->val(), 0, 1);
+      }
+      write_reg(reg_offsets::CF, last_shift_bit->val());
+
+      // if shifting by 1, OF needs to be set as with immediate shift
+      auto one_shift = builder().insert_cmpne(amt->val(), builder().insert_constant_i(amt->val().type(), 1)->val());
+      cond_br_node *one_br = (cond_br_node *)builder().insert_cond_br(one_shift->val(), nullptr);
+
+      if (inst == XED_ICLASS_SHL) {
+        // for left shifts, OF is set to 0 iff most significant bit of the result equals CF
+        auto msb = builder().insert_bit_extract(shift->val(), shift->val().type().width() - 1, 1);
+        auto of = builder().insert_xor(last_shift_bit->val(), msb->val());
+        write_reg(reg_offsets::OF, of->val());
+      } else if (inst == XED_ICLASS_SAR) {
+        // for SAR, OF is cleared
+        write_reg(reg_offsets::OF, builder().insert_constant_u8(0)->val());
+      } else {
+        // for SHR, OF is set to most significant bit of original operand
+        auto msb = builder().insert_bit_extract(src->val(), src->val().type().width() - 1, 1);
+        write_reg(reg_offsets::OF, msb->val());
+      }
+
+      auto end_label = builder().insert_label("end");
+      zero_br->add_br_target(end_label);
+      one_br->add_br_target(end_label);
+    }
 		break;
+  }
 
 	case XED_ICLASS_PSLLDQ: {
     auto amt_val = ((constant_node *)amt)->const_val_i();
