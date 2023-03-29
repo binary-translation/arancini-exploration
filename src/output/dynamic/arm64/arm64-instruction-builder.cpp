cd8e5b6327731734c847a026c241fbd8b2e1b6a7
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-03-29 21:30:50 +0200

    add code

--- src/output/dynamic/arm64/arm64-instruction-builder.cpp
+++ src/output/dynamic/arm64/arm64-instruction-builder.cpp
@@ -176,7 +176,8 @@ void arm64_instruction_builder::allocate() {
     // SP (x31),
     // FP (x29),
     // zero (x31)
-	std::bitset<32> avail_physregs = 0x5FFFFFFFF;
+    // Return to trampoline (x30)
+	std::bitset<32> avail_physregs = 0x1FFFFFFFF;
 
 	for (auto RI = instructions_.rbegin(), RE = instructions_.rend(); RI != RE; RI++) {
 		auto &insn = *RI;
