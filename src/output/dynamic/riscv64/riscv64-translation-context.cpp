380098d35ff38543f8fcc6c25c345acde8b6d276
Simon Kammermeier <simon.kammermeier@tum.de>
2023-04-11 20:35:34 +0200

    [RV64] 4x32bit vector add

--- src/output/dynamic/riscv64/riscv64-translation-context.cpp
+++ src/output/dynamic/riscv64/riscv64-translation-context.cpp
@@ -538,7 +538,7 @@ Register riscv64_translation_context::materialise_cast(const cast_node &n)
 {
 	Register src_reg = std::get<Register>(materialise(n.source_value().owner()));
 
-	bool works = (is_scalar_int(n.val()) || (is_int_vector(n.val(), 128, 64) && n.op() == cast_op::bitcast))
+	bool works = (is_scalar_int(n.val()) || ((is_int_vector(n.val(), 128, 64) || is_int_vector(n.val(), 128, 32)) && n.op() == cast_op::bitcast))
 		&& (is_gpr_or_flag(n.source_value()) || (is_i128(n.source_value()) && (n.op() == cast_op::trunc || n.op() == cast_op::bitcast)));
 	if (!works) {
 		throw std::runtime_error("unsupported types on cast operation");
@@ -770,7 +770,7 @@ void riscv64_translation_context::materialise_write_reg(const write_reg_node &n)
 			(assembler_.*store_instr)(reg, { FP, static_cast<intptr_t>(n.regoff()) });
 		}
 		return;
-	} else if (is_i128(value) || is_int_vector(value, 128, 64)) {
+	} else if (is_i128(value) || is_int_vector(value, 128, 64) || is_int_vector(value, 128, 32)) {
 		Register reg = std::get<Register>(materialise(value.owner())); // Will give lower 64bit
 		Register reg2 = get_secondary_register(&value); // Will give higher 64bit
 
@@ -1186,7 +1186,8 @@ Register riscv64_translation_context::materialise_binary_arith(const binary_arit
 {
 	bool works = (is_gpr_or_flag(n.val()) && is_gpr_or_flag(n.lhs()) && is_gpr_or_flag(n.rhs()))
 		|| ((n.op() == binary_arith_op::mul || n.op() == binary_arith_op::div || n.op() == binary_arith_op::mod || n.op() == binary_arith_op::bxor)
-			&& is_i128(n.val()) && is_i128(n.lhs()) && is_i128(n.rhs()));
+			&& is_i128(n.val()) && is_i128(n.lhs()) && is_i128(n.rhs()))
+		|| ((is_int_vector(n.val(), 128, 32)) && n.op() == binary_arith_op::add);
 	if (!works) {
 		throw std::runtime_error("unsupported width on binary arith operation");
 	}
@@ -1294,6 +1295,36 @@ standardPath:
 
 	case binary_arith_op::add:
 		switch (n.val().type().width()) {
+		case 128: {
+			if (is_int_vector(n.val(), 128, 32)) {
+				Register src_reg22 = get_secondary_register(&n.rhs());
+				Register src_reg12 = get_secondary_register(&n.rhs());
+				Register out_reg2 = get_secondary_register(&n.val());
+
+				assembler_.srli(CF, src_reg1, 32);
+				assembler_.srli(OF, src_reg2, 32);
+				assembler_.add(OF, OF, CF);
+				assembler_.slli(OF, OF, 32);
+
+				assembler_.add(out_reg, src_reg1, src_reg2);
+				assembler_.slli(out_reg, out_reg, 32);
+				assembler_.srli(out_reg, out_reg, 32);
+
+				assembler_.or_(out_reg, out_reg, OF);
+
+				assembler_.srli(CF, src_reg12, 32);
+				assembler_.srli(OF, src_reg22, 32);
+				assembler_.add(OF, OF, CF);
+				assembler_.slli(OF, OF, 32);
+
+				assembler_.add(out_reg2, src_reg12, src_reg22);
+				assembler_.slli(out_reg2, out_reg2, 32);
+				assembler_.srli(out_reg2, out_reg2, 32);
+
+				assembler_.or_(out_reg2, out_reg2, OF);
+			}
+			// Else Should not happen
+		} break;
 		case 64:
 			assembler_.add(out_reg, src_reg1, src_reg2);
 			break;
