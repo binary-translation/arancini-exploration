9853d552d3269f639c91191bfd6b5a5d1b87e360
Simon Kammermeier <simon.kammermeier@tum.de>
2023-05-10 18:22:38 +0200

    [runtime] Support native syscalls on x86.

--- inc/arancini/runtime/exec/native_syscall.h
+++ inc/arancini/runtime/exec/native_syscall.h
@@ -74,9 +74,7 @@ template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, u
 	__asm__ __volatile__("ecall\n\t" : "=r"(a0) : "r"(a7), "0"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5) : "memory");
 	return a0;
 }
-#endif // ARCH_RISCV64
-
-#if defined(ARCH_AARCH64)
+#elif defined(ARCH_AARCH64)
 template <> inline uint64_t native_syscall(uint64_t syscall_no)
 {
 	register uint64_t x0 __asm__("x0");
@@ -157,5 +155,62 @@ template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, u
                          : "memory");
 	return x0;
 }
-#endif // ARCH_AARCH64
+#elif defined(ARCH_X86_64)
 
+template <> inline uint64_t native_syscall(uint64_t syscall_no)
+{
+	uint64_t retval = syscall_no;
+	__asm__ volatile("syscall" : "+a"(retval) : : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1)
+{
+	uint64_t retval = syscall_no;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1) : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, uint64_t arg2)
+{
+	uint64_t retval = syscall_no;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1), "S"(arg2) : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, uint64_t arg2, uint64_t arg3)
+{
+	uint64_t retval = syscall_no;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1), "S"(arg2), "d"(arg3) : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)
+{
+	uint64_t retval = syscall_no;
+	register uint64_t r10 __asm__("r10") = arg4;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10) : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5)
+{
+	uint64_t retval = syscall_no;
+	register uint64_t r10 __asm__("r10") = arg4;
+	register uint64_t r8 __asm__("r8") = arg5;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10), "r"(r8) : "memory", "rcx", "r11");
+	return retval;
+}
+
+template <> inline uint64_t native_syscall(uint64_t syscall_no, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6)
+{
+	uint64_t retval = syscall_no;
+	register uint64_t r8 __asm__("r8") = arg5;
+	register uint64_t r9 __asm__("r9") = arg6;
+	register uint64_t r10 __asm__("r10") = arg4;
+	__asm__ volatile("syscall" : "+a"(retval) : "D"(arg1), "S"(arg2), "d"(arg3), "r"(r8), "r"(r9), "r"(r10) : "memory", "rcx", "r11");
+	return retval;
+}
+#else
+#error Unsupported arch in native_syscall.h
+#endif
