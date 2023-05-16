e547e53b8c7e70eb13be63dd9f07ede8f920f826
ReimersS <sebastian_reimers@web.de>
2023-05-16 15:50:50 +0200

    [backend-llvm] Rebase 2|3 Fix double cases in Main switch

--- inc/arancini/output/static/llvm/llvm-static-output-engine-impl.h
+++ inc/arancini/output/static/llvm/llvm-static-output-engine-impl.h
@@ -103,7 +103,7 @@ private:
 	void optimise();
 	void compile();
 	void lower_chunks(::llvm::SwitchInst *pcswitch, ::llvm::BasicBlock *contblock);
-	void lower_chunk(::llvm::SwitchInst *pcswitch, ::llvm::BasicBlock *contblock, std::shared_ptr<ir::chunk> chunk);
+	void lower_chunk(::llvm::SwitchInst *pcswitch, ::llvm::BasicBlock *contblock, std::shared_ptr<ir::chunk> chunk, std::shared_ptr<std::map<unsigned long, ::llvm::BasicBlock *>> blocks);
 	::llvm::Value *lower_node(::llvm::IRBuilder<::llvm::ConstantFolder, ::llvm::IRBuilderDefaultInserter> &builder, ::llvm::Argument *start_arg,
 		std::shared_ptr<ir::packet> pkt, ir::node *a);
 	::llvm::Value *lower_port(::llvm::IRBuilder<::llvm::ConstantFolder, ::llvm::IRBuilderDefaultInserter> &builder, ::llvm::Argument *start_arg,
