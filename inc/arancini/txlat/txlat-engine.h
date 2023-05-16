b0fee18f7763082e405581aec31390397d0d9158
Redha Gouicem <redha.gouicem@gmail.com>
2023-05-16 17:16:23 +0200

    add optimisation pass that removes unused flags (zf, of, ...)

--- inc/arancini/txlat/txlat-engine.h
+++ inc/arancini/txlat/txlat-engine.h
@@ -29,5 +29,7 @@ public:
 private:
 	void process_options(arancini::output::o_static::static_output_engine &oe, const boost::program_options::variables_map &cmdline);
 	std::shared_ptr<ir::chunk> translate_symbol(arancini::input::input_arch &ia, elf::elf_reader &reader, const elf::symbol &sym);
+  void generate_dot_graph(arancini::output::o_static::static_output_engine &oe, std::string filename);
+  void optimise(arancini::output::o_static::static_output_engine &oe, const boost::program_options::variables_map &cmdline);
 };
 } // namespace arancini::txlat
