60a08ca5fd437c1c6a7d0f8988e8823774033611
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-05-27 15:43:51 +0200

    [AARCH64] Refactor instruction builder

--- inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
+++ inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
@@ -14,180 +14,205 @@ public:
 	void add(const operand &dst,
              const operand &src1,
              const operand &src2) {
-        append(instruction::add(dst, src1, src2));
+        append(instruction("add", def(dst), use(src1), use(src2)));
     }
 
-	void add(const operand &dst,
-             const operand &src1,
-             const operand &src2,
-             const operand &shift) {
-        append(instruction::add(dst, src1, src2, shift));
+    void add(const operand &dst,
+                           const operand &src1,
+                           const operand &src2,
+                           const operand &shift) {
+        append(instruction("add", def(dst), use(src1), use(src2), use(shift)));
     }
 
-	void sub(const operand &dst,
-             const operand &src1,
-             const operand &src2) {
-        append(instruction::sub(dst, src1, src2));
+    void sub(const operand &dst,
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("sub", def(dst), use(src1), use(src2)));
     }
 
-	void sub(const operand &dst,
-             const operand &src1,
-             const operand &src2,
-             const operand &shift) {
-        append(instruction::sub(dst, src1, src2, shift));
+    void sub(const operand &dst,
+                           const operand &src1,
+                           const operand &src2,
+                           const operand &shift) {
+        append(instruction("sub", def(dst), use(src1), use(src2), use(shift)));
     }
 
-	void or_(const operand &dst,
-             const operand &src1,
-             const operand &src2) {
-        append(instruction::or_(dst, src1, src2));
+    void or_(const operand &dst,
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("orr", def(dst), use(src1), use(src2)));
     }
 
-	void and_(const operand &dst,
-              const operand &src1,
-              const operand &src2) {
-        append(instruction::and_(dst, src1, src2));
+    void and_(const operand &dst,
+                            const operand &src1,
+                            const operand &src2) {
+        append(instruction("and", def(dst), use(src1), use(src2)));
     }
 
-	void xor_(const operand &dst,
-              const operand &src1,
-              const operand &src2) {
-        append(instruction::xor_(dst, src1, src2));
+    void xor_(const operand &dst,
+                            const operand &src1,
+                            const operand &src2) {
+        append(instruction("eor", def(dst), use(src1), use(src2)));
     }
 
-	void not_(const operand &dst, const operand &src) {
-        append(instruction::not_(dst, src));
+    void not_(const operand &dst, const operand &src) {
+        append(instruction("mvn", def(dst), use(src)));
+    }
+
+    void moveq(const operand &dst, const immediate_operand &src) {
+        append(instruction("moveq", def(dst), use(src)));
+    }
+
+    void movss(const operand &dst, const immediate_operand &src) {
+        append(instruction("movss", def(dst), use(src)));
+    }
+
+    // TODO: express dependency on flag reg
+    void movvs(const operand &dst, const immediate_operand &src) {
+        append(instruction("movvs", def(dst), use(src)));
+    }
+
+    void movcs(const operand &dst, const immediate_operand &src) {
+        append(instruction("movcs", def(dst), use(src)));
     }
 
     void movn(const operand &dst,
-              const operand &src,
-              const operand &shift) {
-        append(instruction::movn(dst, src, shift));
+                            const operand &src,
+                            const operand &shift) {
+        append(instruction("movn", def(dst), use(src), use(shift)));
     }
 
     void movz(const operand &dst,
-              const operand &src,
-              const operand &shift) {
-        append(instruction::movz(dst, src, shift));
+                            const operand &src,
+                            const operand &shift) {
+        append(instruction("movz", def(dst), use(src), use(shift)));
     }
 
     void movk(const operand &dst,
-              const operand &src,
-              const operand &shift) {
-        append(instruction::movk(dst, src, shift));
+                            const operand &src,
+                            const operand &shift) {
+        append(instruction("movk", def(dst), use(src), use(shift)));
     }
 
-    void mov(const operand &dst, const operand &src) { append(instruction::mov(dst, src)); }
-
-	void setz(const operand &dst) {
-        append(instruction::cset(dst, cond_operand("eq")));
+    void mov(const operand &dst, const operand &src) {
+        append(instruction("mov", def(dst), use(src)));
     }
 
-	void sets(const operand &dst) {
-        append(instruction::cset(dst, cond_operand("lt")));
+    void b(const std::string &src) {
+        append(instruction("b", use(label_operand(src))));
     }
 
-	void setc(const operand &dst) {
-        append(instruction::cset(dst, cond_operand("cs")));
+    void beq(const std::string &src) {
+        append(instruction("beq", use(label_operand(src))));
     }
 
-	void seto(const operand &dst) {
-        append(instruction::cset(dst, cond_operand("vs")));
+    void bl(const std::string &name) {
+        append(instruction("bl", use(label_operand(name))));
     }
 
-    void b(const std::string &label) {
-        append(instruction::b(label));
-    }
-
-    void beq(const std::string &label) {
-        append(instruction::beq(label));
-    }
-
-    void label(const std::string &name) {
-        append(instruction::label(name));
+    void cmp(const operand &dst,
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("cmp", def(dst), use(src1), use(src2)));
     }
 
     void lsl(const operand &dst,
-             const operand &input,
-             const operand &amount) {
-        append(instruction::lsl(dst, input, amount));
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("lsl", def(dst), use(src1), use(src2)));
     }
 
     void lsr(const operand &dst,
-             const operand &input,
-             const operand &amount) {
-        append(instruction::lsr(dst, input, amount));
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("lsr", def(dst), use(src1), use(src2)));
     }
 
     void asr(const operand &dst,
-             const operand &input,
-             const operand &amount) {
-        append(instruction::asr(dst, input, amount));
+                           const operand &src1,
+                           const operand &src2) {
+        append(instruction("asr", def(dst), use(src1), use(src2)));
     }
 
     void csel(const operand &dst,
-              const operand &src1,
-              const operand &src2,
-              const operand &cond) {
-        append(instruction::csel(dst, src1, src2, cond));
+                            const operand &src1,
+                            const operand &src2,
+                            const operand &cond) {
+        append(instruction("csel", def(dst), use(src1), use(src2), use(cond)));
     }
 
-    void csel(const operand &dst,
+    void cset(const operand &dst,
               const operand &cond) {
-        append(instruction::cset(dst, cond));
+        append(instruction("cset", def(dst), use(cond)));
     }
 
     void ubfx(const operand &dst,
               const operand &src1,
               const operand &src2,
               const operand &cond) {
-        append(instruction::ubfx(dst, src1, src2, cond));
+        append(instruction("ubfx", def(dst), use(src1), use(src2), use(cond)));
     }
 
     void bfi(const operand &dst,
              const operand &src1,
              const operand &src2,
              const operand &cond) {
-        append(instruction::bfi(dst, src1, src2, cond));
+        append(instruction("bfi", def(dst), use(src1), use(src2), use(cond)));
     }
 
     void ldr(const operand &dst,
              const operand &base) {
-        append(instruction::ldr(dst, base));
+        append(instruction("ldr", def(dst), use(base)));
     }
 
-    void str(const operand &dst,
+    void str(const operand &src,
              const operand &base) {
-        append(instruction::str(dst, base));
+        append(instruction("str", use(src), def(base)));
     }
 
-    void mul(const operand &dst,
+    void mul(const operand &dest,
              const operand &src1,
              const operand &src2) {
-        append(instruction::mul(dst, src1, src2));
+        append(instruction("mul", def(dest), use(src1), use(src2)));
     }
 
-    void sdiv(const operand &dst,
+    void sdiv(const operand &dest,
               const operand &src1,
               const operand &src2) {
-        append(instruction::sdiv(dst, src1, src2));
+        append(instruction("sdiv", def(dest), use(src1), use(src2)));
     }
 
     void cmp(const operand &src1,
              const operand &src2) {
-        append(instruction::cmp(src1, src2));
-    }
-
-    void bl(const std::string &n) {
-        append(instruction::bl(n));
+        append(instruction("cmp", use(src1), use(src2)));
     }
 
     void ret() {
-        append(instruction::ret());
+        append(instruction("ret"));
     }
 
     void brk(const operand &imm) {
-        append(instruction::brk(imm));
+        append(instruction("brk", use(imm)));
+    }
+
+    void label(const std::string &label) {
+        append(instruction(label + ":"));
+    }
+
+	void setz(const operand &dst) {
+        append(instruction("cset", def(dst), cond_operand("eq")));
+    }
+
+	void sets(const operand &dst) {
+        append(instruction("cset", def(dst), cond_operand("lt")));
+    }
+
+	void setc(const operand &dst) {
+        append(instruction("cset", def(dst), cond_operand("cs")));
+    }
+
+	void seto(const operand &dst) {
+        append(instruction("cset", def(dst), cond_operand("vs")));
     }
 
     // TODO: insert separators before/after instructions
