aa3cea63d8c239a3180139d28087d81a237f4e8a
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-05-27 15:23:01 +0200

    [AARCH64] Refactor naming

--- inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
+++ inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
@@ -9,185 +9,185 @@
 #include <unordered_map>
 
 namespace arancini::output::dynamic::arm64 {
-class arm64_instruction_builder {
+class instruction_builder {
 public:
-	void add(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2) {
-        append(arm64_instruction::add(dst, src1, src2));
+	void add(const operand &dst,
+             const operand &src1,
+             const operand &src2) {
+        append(instruction::add(dst, src1, src2));
     }
 
-	void add(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2,
-             const arm64_operand &shift) {
-        append(arm64_instruction::add(dst, src1, src2, shift));
+	void add(const operand &dst,
+             const operand &src1,
+             const operand &src2,
+             const operand &shift) {
+        append(instruction::add(dst, src1, src2, shift));
     }
 
-	void sub(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2) {
-        append(arm64_instruction::sub(dst, src1, src2));
+	void sub(const operand &dst,
+             const operand &src1,
+             const operand &src2) {
+        append(instruction::sub(dst, src1, src2));
     }
 
-	void sub(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2,
-             const arm64_operand &shift) {
-        append(arm64_instruction::sub(dst, src1, src2, shift));
+	void sub(const operand &dst,
+             const operand &src1,
+             const operand &src2,
+             const operand &shift) {
+        append(instruction::sub(dst, src1, src2, shift));
     }
 
-	void or_(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2) {
-        append(arm64_instruction::or_(dst, src1, src2));
+	void or_(const operand &dst,
+             const operand &src1,
+             const operand &src2) {
+        append(instruction::or_(dst, src1, src2));
     }
 
-	void and_(const arm64_operand &dst,
-              const arm64_operand &src1,
-              const arm64_operand &src2) {
-        append(arm64_instruction::and_(dst, src1, src2));
+	void and_(const operand &dst,
+              const operand &src1,
+              const operand &src2) {
+        append(instruction::and_(dst, src1, src2));
     }
 
-	void xor_(const arm64_operand &dst,
-              const arm64_operand &src1,
-              const arm64_operand &src2) {
-        append(arm64_instruction::xor_(dst, src1, src2));
+	void xor_(const operand &dst,
+              const operand &src1,
+              const operand &src2) {
+        append(instruction::xor_(dst, src1, src2));
     }
 
-	void not_(const arm64_operand &dst, const arm64_operand &src) {
-        append(arm64_instruction::not_(dst, src));
+	void not_(const operand &dst, const operand &src) {
+        append(instruction::not_(dst, src));
     }
 
-    void movn(const arm64_operand &dst,
-              const arm64_operand &src,
-              const arm64_operand &shift) {
-        append(arm64_instruction::movn(dst, src, shift));
+    void movn(const operand &dst,
+              const operand &src,
+              const operand &shift) {
+        append(instruction::movn(dst, src, shift));
     }
 
-    void movz(const arm64_operand &dst,
-              const arm64_operand &src,
-              const arm64_operand &shift) {
-        append(arm64_instruction::movz(dst, src, shift));
+    void movz(const operand &dst,
+              const operand &src,
+              const operand &shift) {
+        append(instruction::movz(dst, src, shift));
     }
 
-    void movk(const arm64_operand &dst,
-              const arm64_operand &src,
-              const arm64_operand &shift) {
-        append(arm64_instruction::movk(dst, src, shift));
+    void movk(const operand &dst,
+              const operand &src,
+              const operand &shift) {
+        append(instruction::movk(dst, src, shift));
     }
 
-    void mov(const arm64_operand &dst, const arm64_operand &src) { append(arm64_instruction::mov(dst, src)); }
+    void mov(const operand &dst, const operand &src) { append(instruction::mov(dst, src)); }
 
-	void setz(const arm64_operand &dst) {
-        append(arm64_instruction::cset(dst, arm64_cond_operand("eq")));
+	void setz(const operand &dst) {
+        append(instruction::cset(dst, cond_operand("eq")));
     }
 
-	void sets(const arm64_operand &dst) {
-        append(arm64_instruction::cset(dst, arm64_cond_operand("lt")));
+	void sets(const operand &dst) {
+        append(instruction::cset(dst, cond_operand("lt")));
     }
 
-	void setc(const arm64_operand &dst) {
-        append(arm64_instruction::cset(dst, arm64_cond_operand("cs")));
+	void setc(const operand &dst) {
+        append(instruction::cset(dst, cond_operand("cs")));
     }
 
-	void seto(const arm64_operand &dst) {
-        append(arm64_instruction::cset(dst, arm64_cond_operand("vs")));
+	void seto(const operand &dst) {
+        append(instruction::cset(dst, cond_operand("vs")));
     }
 
     void b(const std::string &label) {
-        append(arm64_instruction::b(label));
+        append(instruction::b(label));
     }
 
     void beq(const std::string &label) {
-        append(arm64_instruction::beq(label));
+        append(instruction::beq(label));
     }
 
     void label(const std::string &name) {
-        append(arm64_instruction::label(name));
+        append(instruction::label(name));
     }
 
-    void lsl(const arm64_operand &dst,
-             const arm64_operand &input,
-             const arm64_operand &amount) {
-        append(arm64_instruction::lsl(dst, input, amount));
+    void lsl(const operand &dst,
+             const operand &input,
+             const operand &amount) {
+        append(instruction::lsl(dst, input, amount));
     }
 
-    void lsr(const arm64_operand &dst,
-             const arm64_operand &input,
-             const arm64_operand &amount) {
-        append(arm64_instruction::lsr(dst, input, amount));
+    void lsr(const operand &dst,
+             const operand &input,
+             const operand &amount) {
+        append(instruction::lsr(dst, input, amount));
     }
 
-    void asr(const arm64_operand &dst,
-             const arm64_operand &input,
-             const arm64_operand &amount) {
-        append(arm64_instruction::asr(dst, input, amount));
+    void asr(const operand &dst,
+             const operand &input,
+             const operand &amount) {
+        append(instruction::asr(dst, input, amount));
     }
 
-    void csel(const arm64_operand &dst,
-              const arm64_operand &src1,
-              const arm64_operand &src2,
-              const arm64_operand &cond) {
-        append(arm64_instruction::csel(dst, src1, src2, cond));
+    void csel(const operand &dst,
+              const operand &src1,
+              const operand &src2,
+              const operand &cond) {
+        append(instruction::csel(dst, src1, src2, cond));
     }
 
-    void csel(const arm64_operand &dst,
-              const arm64_operand &cond) {
-        append(arm64_instruction::cset(dst, cond));
+    void csel(const operand &dst,
+              const operand &cond) {
+        append(instruction::cset(dst, cond));
     }
 
-    void ubfx(const arm64_operand &dst,
-              const arm64_operand &src1,
-              const arm64_operand &src2,
-              const arm64_operand &cond) {
-        append(arm64_instruction::ubfx(dst, src1, src2, cond));
+    void ubfx(const operand &dst,
+              const operand &src1,
+              const operand &src2,
+              const operand &cond) {
+        append(instruction::ubfx(dst, src1, src2, cond));
     }
 
-    void bfi(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2,
-             const arm64_operand &cond) {
-        append(arm64_instruction::bfi(dst, src1, src2, cond));
+    void bfi(const operand &dst,
+             const operand &src1,
+             const operand &src2,
+             const operand &cond) {
+        append(instruction::bfi(dst, src1, src2, cond));
     }
 
-    void ldr(const arm64_operand &dst,
-             const arm64_operand &base) {
-        append(arm64_instruction::ldr(dst, base));
+    void ldr(const operand &dst,
+             const operand &base) {
+        append(instruction::ldr(dst, base));
     }
 
-    void str(const arm64_operand &dst,
-             const arm64_operand &base) {
-        append(arm64_instruction::str(dst, base));
+    void str(const operand &dst,
+             const operand &base) {
+        append(instruction::str(dst, base));
     }
 
-    void mul(const arm64_operand &dst,
-             const arm64_operand &src1,
-             const arm64_operand &src2) {
-        append(arm64_instruction::mul(dst, src1, src2));
+    void mul(const operand &dst,
+             const operand &src1,
+             const operand &src2) {
+        append(instruction::mul(dst, src1, src2));
     }
 
-    void sdiv(const arm64_operand &dst,
-              const arm64_operand &src1,
-              const arm64_operand &src2) {
-        append(arm64_instruction::sdiv(dst, src1, src2));
+    void sdiv(const operand &dst,
+              const operand &src1,
+              const operand &src2) {
+        append(instruction::sdiv(dst, src1, src2));
     }
 
-    void cmp(const arm64_operand &src1,
-             const arm64_operand &src2) {
-        append(arm64_instruction::cmp(src1, src2));
+    void cmp(const operand &src1,
+             const operand &src2) {
+        append(instruction::cmp(src1, src2));
     }
 
     void bl(const std::string &n) {
-        append(arm64_instruction::bl(n));
+        append(instruction::bl(n));
     }
 
     void ret() {
-        append(arm64_instruction::ret());
+        append(instruction::ret());
     }
 
-    void brk(const arm64_operand &imm) {
-        append(arm64_instruction::brk(imm));
+    void brk(const operand &imm) {
+        append(instruction::brk(imm));
     }
 
     // TODO: insert separators before/after instructions
@@ -196,7 +196,7 @@ public:
     bool has_label(const std::string &label) {
         auto insn = instructions_;
         return std::any_of(insn.rbegin(), insn.rend(),
-                            [&](const arm64_instruction &i) {
+                            [&](const instruction &i) {
                                 return i.opcode == label;
                             });
     }
@@ -209,9 +209,9 @@ public:
 
 	size_t nr_instructions() const { return instructions_.size(); }
 private:
-	std::vector<arm64_instruction> instructions_;
+	std::vector<instruction> instructions_;
 
-	void append(const arm64_instruction &i) { instructions_.push_back(i); }
+	void append(const instruction &i) { instructions_.push_back(i); }
 
     void spill();
 };
