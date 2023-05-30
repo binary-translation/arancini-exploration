fc7c1bc80ef54f4ead50a1cac0b8f70cdf637fa8
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-05-31 00:17:44 +0200

    [AARCH64] Refactor stack printing code

--- src/runtime/exec/execution-context.cpp
+++ src/runtime/exec/execution-context.cpp
@@ -135,7 +135,7 @@ int execution_context::invoke(void *cpu_state)
     auto* memptr = reinterpret_cast<uint64_t*>(get_memory_ptr(0) + x86_state->RSP);
     std::cerr << "--------------------------------------------\n";
     std::cerr << "STACK:\n";
-    x86::print_stack(std::cerr, memptr, 5);
+    x86::print_stack(std::cerr, memptr, 20);
     std::cerr << "--------------------------------------------\n";
 #endif
 
