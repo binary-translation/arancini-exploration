dc0465376b6ed778b0fbe5a99b6a40af68fc0656
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-05-30 18:41:11 +0200

    [AARCH64] Add stack printer code

--- src/runtime/exec/execution-context.cpp
+++ src/runtime/exec/execution-context.cpp
@@ -137,6 +137,11 @@ int execution_context::invoke(void *cpu_state)
 
 #ifndef NDEBUG
     std::cerr << *x86_state;
+    auto* memptr = reinterpret_cast<uint64_t*>(get_memory_ptr(0) + x86_state->RSP);
+    std::cerr << "--------------------------------------------\n";
+    std::cerr << "STACK:\n";
+    x86::print_stack(std::cerr, memptr, 5);
+    std::cerr << "--------------------------------------------\n";
 #endif
 
 	auto txln = te_.get_translation(x86_state->PC);
