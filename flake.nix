15ebbe480524532685a23df77b628645e59c60c3
ReimersS <sebastian.reimers@tum.de>
2023-06-14 16:47:51 +0000

    flake.nix Add riscv

--- flake.nix
+++ flake.nix
@@ -9,9 +9,9 @@
 
 	# output format guide https://nixos.wiki/wiki/Flakes#Output_schema
 	outputs = { self, nixpkgs, flake-utils, ... }:
-	flake-utils.lib.eachDefaultSystem (system:
+	flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "riscv64-linux" ] (system:
 	let
-		pkgs =
+		all_pkgs =
 			if system == "aarch64-linux" then
 				import nixpkgs { inherit system; overlays = [
 					(final: prev: { xed = prev.xed.overrideAttrs (oldAttr: rec {
@@ -23,6 +23,11 @@
 					})];}
 			else
 				import nixpkgs { inherit system; };
+		pkgs =
+			if system == "riscv64-linux" then
+				all_pkgs.pkgsCross.riscv64
+			else
+				all_pkgs;
 	in
 	{
 		defaultPackage =
