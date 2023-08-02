144260c5bc3d0262f4e0a7aafa50bdc763f899a9
Simon Kammermeier <simon.kammermeier@tum.de>
2023-08-02 12:59:59 +0200

    Fix fadec.cmake

--- cmake/fadec.cmake
+++ cmake/fadec.cmake
@@ -7,16 +7,41 @@ set(FADEC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/fadec)
 
 find_package(Python3 COMPONENTS Interpreter)
 if (NOT Python3_FOUND)
-    message (FATAL_ERROR "Python3 is required for building fadec")
+    message(FATAL_ERROR "Python3 is required for building fadec")
 endif ()
 
+if (NOT EXISTS ${FADEC_DIR}/encode.c)
+    find_package(Git QUIET)
+    if (NOT Git_FOUND OR NOT EXISTS "${CMAKE_SOURCE_DIR}/.git")
+        # Fetch FADEC from repo
+        message(STATUS "Git not found: fetching release")
+
+        FetchContent_Declare(FADEC
+                URL https://github.com/aengelke/fadec/archive/refs/heads/main.zip
+                DOWNLOAD_DIR ${FADEC_DIR}/../
+                SOURCE_DIR ${FADEC_DIR}
+        )
+
+
+    else ()
+        # Get submodule: fadec
+        FetchContent_Declare(FADEC
+                DOWNLOAD_COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive ${FADEC_DIR}
+        )
+    endif ()
+    # Populate FADEC directory
+    FetchContent_MakeAvailable(FADEC)
+endif ()
+
+
+
 add_custom_command(OUTPUT ${FADEC_DIR}/fadec-encode-public.inc ${FADEC_DIR}/fadec-encode-private.inc
-    COMMAND ${Python3_EXECUTABLE} ${FADEC_DIR}/parseinstrs.py encode ${FADEC_DIR}/instrs.txt ${FADEC_DIR}/fadec-encode-public.inc ${FADEC_DIR}/fadec-encode-private.inc
+        COMMAND ${Python3_EXECUTABLE} ${FADEC_DIR}/parseinstrs.py encode ${FADEC_DIR}/instrs.txt ${FADEC_DIR}/fadec-encode-public.inc ${FADEC_DIR}/fadec-encode-private.inc
 )
 
 add_custom_target(fadec-defs ALL DEPENDS ${FADEC_DIR}/fadec-encode-public.inc ${FADEC_DIR}/fadec-encode-private.inc)
 
-add_library(fadec ../lib/fadec/encode.c)
+add_library(fadec ${FADEC_DIR}/encode.c)
 
-target_include_directories(fadec PUBLIC lib/fadec)
+target_include_directories(fadec PUBLIC ${FADEC_DIR})
 add_dependencies(fadec fadec-defs)
