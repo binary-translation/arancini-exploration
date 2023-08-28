669afa1bdfa2e0d4aa610fb12ade8509b6e9065a
Simon Kammermeier <simon.kammermeier@tum.de>
2023-08-28 16:59:21 +0200

    [RV64] Migrate bit_insert to TypedRegister

--- inc/arancini/output/dynamic/riscv64/bitwise.h
+++ inc/arancini/output/dynamic/riscv64/bitwise.h
@@ -39,3 +39,35 @@ inline void bit_extract(Assembler &assembler, TypedRegister &out, const TypedReg
 	out.set_actual_width();
 	out.set_type(value_type::u64());
 }
+
+inline void bit_insert(
+	Assembler &assembler, TypedRegister &out, const TypedRegister& src, const TypedRegister& bits, const int to, const int length, const TypedRegister& temp_reg)
+{
+	int64_t mask = ~(((1ll << length) - 1) << to);
+
+	if (!out.type().is_vector() && out.type().is_integer()) {
+		const Register &src1 = out.type().element_width() == 128 ? src.reg1() : src;
+		const Register &out1 = out.type().element_width() == 128 ? out.reg1() : out;
+		//TODO Might be able to save masking of src or bits in some cases
+		if (to == 0 && IsITypeImm(mask)) {
+			// Since to==0 no shift necessary and just masking both is enough
+			// `~mask` also fits IType since `mask` has all but lower bits set
+			assembler.andi(temp_reg, bits, ~mask);
+			assembler.andi(out1, src1, mask);
+		} else {
+			gen_constant(assembler, mask, temp_reg);
+			assembler.and_(out1, src1, temp_reg);
+			assembler.slli(temp_reg, bits, 64 - length);
+			if (length + to != 64) {
+				assembler.srli(temp_reg, temp_reg, 64 - (length + to));
+			}
+
+			// Might be able to set "bigger" out type here, but rather careful by keeping intended/explicit out type
+		}
+
+		assembler.or_(out1, out1, temp_reg);
+
+	} else {
+		throw std::runtime_error("not implemented");
+	}
+}
