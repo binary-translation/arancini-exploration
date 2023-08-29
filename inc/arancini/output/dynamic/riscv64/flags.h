f814e6f2d5fcde801fefdbbfe17a2e77c4e5bb84
Simon Kammermeier <simon.kammermeier@tum.de>
2023-08-29 13:03:04 +0200

    [RV64] Migrate immediate addition to TypedRegister

--- inc/arancini/output/dynamic/riscv64/flags.h
+++ inc/arancini/output/dynamic/riscv64/flags.h
@@ -28,6 +28,42 @@ void zero_sign_flag(Assembler &assembler, TypedRegister &out, bool z, bool n)
 
 /**
  * Generate zero, overflow, carry and sign flags based on the result in the given register and the given input register and immediate assuming the operation was
+ * an addition.
+ * @param z whether zero flag should be generated
+ * @param v whether overflow flag should be generated
+ * @param c whether carry flag should be generated
+ * @param n whether sign flag should be generated
+ * @param of Optional. Register override for overflow flag
+ * @param cf Optional. Register override for carry flag
+ */
+inline void addi_flags(
+	Assembler &assembler, TypedRegister &out, TypedRegister &lhs, intptr_t imm, bool z, bool v, bool c, bool n, Register of = OF, Register cf = CF)
+{
+	if (v || c || z || n) {
+		if (!out.type().is_vector() && out.type().is_integer()) {
+			extend_to_64(assembler, out, out);
+			if (v) {
+				extend_to_64(assembler, lhs, lhs);
+
+				assembler.slt(of, out, lhs); // OF FIXME Assumes out!=lhs
+				if (imm < 0) {
+					assembler.xori(of, of, 1); // Invert on negative
+				}
+			}
+			if (c) {
+				extend_to_64(assembler, lhs, lhs);
+
+				assembler.sltu(cf, out, lhs); // CF FIXME Assumes out!=lhs
+			}
+			zero_sign_flag(assembler, out, z, n);
+		} else {
+			throw std::runtime_error("not implemented");
+		}
+	}
+}
+
+/**
+ * Generate zero, overflow, carry and sign flags based on the result in the given register and the given input register and immediate assuming the operation was
  * a subtraction.
  * @param z whether zero flag should be generated
  * @param v whether overflow flag should be generated
