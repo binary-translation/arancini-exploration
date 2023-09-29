a56e587fe48032102065d64e3bba4c46ab7a09d4
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-09-29 21:26:05 +0200

    [AARCH64] Refactor

--- src/output/dynamic/arm64/arm64-instruction.cpp
+++ src/output/dynamic/arm64/arm64-instruction.cpp
@@ -5,6 +5,7 @@
 #include <ostream>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 #include <unordered_set>
 
 using namespace arancini::output::dynamic::arm64;
@@ -110,7 +111,9 @@ size_t assembler::assemble(const char *code, unsigned char **out) {
     size_t size = 0;
     size_t count = 0;
     if (ks_asm(ks_, code, 0, out, &size, &count)) {
-        std::string msg("Keystone assembler encountered error: ");
+        std::string msg("Keystone assembler encountered error after count: ");
+        msg += std::to_string(count);
+        msg += ": ";
         throw std::runtime_error(msg + ks_strerror(ks_errno(ks_)));
     }
 
