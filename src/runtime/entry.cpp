0a5e111d576d9b03318721f3475049b6ef0fd1d4
ReimersS <sebastian.reimers@tum.de>
2023-10-02 15:37:37 +0000

    [backend-llvm] Add memory base to every load/store/atomic on non-x86 archs

--- src/runtime/entry.cpp
+++ src/runtime/entry.cpp
@@ -288,6 +288,7 @@ extern "C" void *initialise_dynamic_runtime(unsigned long entry_point, int argc,
 	__current_state = x86_state;
 	x86_state->PC = entry_point;
 
+	x86_state->GS = (unsigned long long)ctx_->get_memory_ptr(0);
 	x86_state->RSP = setup_guest_stack(argc, argv, 0x100000000, ctx_, start);
 	x86_state->X87_STACK_BASE = (intptr_t)mmap(NULL, 80, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) - (intptr_t)ctx_->get_memory_ptr(0);
 
