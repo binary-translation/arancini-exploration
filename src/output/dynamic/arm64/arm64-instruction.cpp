8890bb486d6f84ec002bb09df4385fb2a465f31d
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-10-08 23:15:46 +0200

    [AARCH64] Implement CMPXCHG support using CAS

--- src/output/dynamic/arm64/arm64-instruction.cpp
+++ src/output/dynamic/arm64/arm64-instruction.cpp
@@ -171,6 +171,10 @@ void operand::dump(std::ostream &os) const {
 		else
 			os << to_string(memory().preg_base());
 
+        if (!memory().offset().value()) {
+            os << "]";
+            break;
+        }
 
         if (!memory().post_index())
             os << ", #0x" << std::hex << memory().offset().value() << ']';
