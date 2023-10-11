5a47571fcf30fc4f716145f297806ba16a9b741b
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-10-11 13:39:49 +0200

    [DEBUG] Implement full logging infrastructure for arancini

--- inc/arancini/input/x86/translators/translators.h
+++ inc/arancini/input/x86/translators/translators.h
@@ -19,12 +19,13 @@ namespace arancini::ir {
 } // namespace arancini::ir
 
 namespace arancini::input::x86::translators {
-  using namespace arancini::ir;
 
-  enum class translation_result { normal, noop, end_of_block, fail };
+using namespace arancini::ir;
 
-  class translator {
-  public:
+enum class translation_result { normal, noop, end_of_block, fail };
+
+class translator {
+public:
     translator(ir_builder &builder)
       : builder_(builder)
     {
@@ -33,8 +34,7 @@ namespace arancini::input::x86::translators {
 	virtual ~translator() = default;
 
 	translation_result translate(off_t address, xed_decoded_inst_t *xed_inst, const std::string &disasm);
-
-  protected:
+protected:
     virtual void do_translate() = 0;
 
     ir_builder &builder() const { return builder_; }
@@ -69,7 +69,7 @@ namespace arancini::input::x86::translators {
 #define DEFREG(ctype, ltype, name) { X86_OFFSET_OF(name), #name },
 #include <arancini/input/x86/reg.def>
 #undef DEFREG
-	};
+    };
 
     action_node *write_reg(reg_offsets reg, port & value);
     value_node *read_reg(const value_type &vt, reg_offsets reg);
@@ -87,15 +87,15 @@ namespace arancini::input::x86::translators {
       set1,
       update
     };
-		/// @brief Write flag register with a flag_op (ignore, set0, set1, update)
-		/// @param op node affecting the flags
-		/// @param zf zero flag (ZF)
-		/// @param cf carry flag (CF)
-		/// @param of overflow floag (OF)
-		/// @param sf sign flag (SF)
-		/// @param pf parity flag (PF)
-		/// @param af adjust flag (AF)
-		void write_flags(value_node * op, flag_op zf, flag_op cf, flag_op of, flag_op sf, flag_op pf, flag_op af);
+    /// @brief Write flag register with a flag_op (ignore, set0, set1, update)
+    /// @param op node affecting the flags
+    /// @param zf zero flag (ZF)
+    /// @param cf carry flag (CF)
+    /// @param of overflow floag (OF)
+    /// @param sf sign flag (SF)
+    /// @param pf parity flag (PF)
+    /// @param af adjust flag (AF)
+    void write_flags(value_node * op, flag_op zf, flag_op cf, flag_op of, flag_op sf, flag_op pf, flag_op af);
 
 
     // x87 FPU stack manipulation
@@ -116,21 +116,20 @@ namespace arancini::input::x86::translators {
     /// @return the action node writng the new top index to the x87 status register
     action_node *fpu_stack_top_move(int val);
 
-		enum class cond_type { nbe, nb, b, be, z, nle, nl, l, le, nz, no, np, ns, o, p, s };
-		value_node *compute_cond(cond_type ct);
+    enum class cond_type { nbe, nb, b, be, z, nle, nl, l, le, nz, no, np, ns, o, p, s };
+    value_node *compute_cond(cond_type ct);
 
-		value_node *auto_cast(const value_type &target_type, value_node *v);
+    value_node *auto_cast(const value_type &target_type, value_node *v);
 
-		value_type type_of_operand(int opnum);
+    value_type type_of_operand(int opnum);
 
     /// @brief Dump the xed operand encoding of the instruction currently being translated
     ///        Use this function when discovering which operands to use when adding support for instructions
     void dump_xed_encoding(void);
-
-	private:
-		ir_builder &builder_;
-		xed_decoded_inst_t *xed_inst_;
-	};
+private:
+    ir_builder &builder_;
+    xed_decoded_inst_t *xed_inst_;
+};
 
 #define DEFINE_TRANSLATOR(name)                 \
 	class name##_translator : public translator { \
@@ -164,3 +163,4 @@ namespace arancini::input::x86::translators {
   DEFINE_TRANSLATOR(control)
   DEFINE_TRANSLATOR(interrupt)
 } // namespace arancini::input::x86::translators
+
