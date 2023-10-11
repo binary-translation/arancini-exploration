c0d24ae32c029d46476d21f833a26369ab2d7737
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2023-10-11 13:39:49 +0200

    [DEBUG] Implement full logging infrastructure for arancini

--- inc/arancini/util/logger.h
+++ inc/arancini/util/logger.h
@@ -6,6 +6,8 @@
 
 namespace utils {
 
+namespace details {
+
 class no_lock_policy {
 public:
     void lock() { }
@@ -20,50 +22,93 @@ private:
     std::mutex mutex_;
 };
 
-template <bool enable, typename lock_policy>
-class logger_impl;
-
-template <typename lock_policy>
-class logger_impl<true, lock_policy> : public lock_policy {
+template <typename T>
+class level_policy {
 public:
-    bool enable(bool status) { enabled_ = status; return enabled_; }
+    enum class levels : uint8_t {
+        disabled,
+        debug,
+        info,
+        warn,
+        error,
+        fatal
+    };
+
+    levels set_level(levels level) {
+        level_ = level;
+        return level_;
+    }
 
     template<typename... Args>
-    void log(Args&&... args) {
+    T &debug(Args&&... args) {
+        if (level_ <= levels::debug)
+            return static_cast<T*>(this)->log(std::forward<Args>(args)...);
+        else return *static_cast<T*>(this);
+    }
+
+    template<typename... Args>
+    T &info(Args&&... args) {
+        if (level_ <= levels::info)
+            return static_cast<T*>(this)->log(std::forward<Args>(args)...);
+        else return *static_cast<T*>(this);
+    }
+
+    template<typename... Args>
+    T &warn(Args&&... args) {
+        if (level_ <= levels::warn)
+            return static_cast<T*>(this)->log(std::forward<Args>(args)...);
+        else return *static_cast<T*>(this);
+    }
+
+    template<typename... Args>
+    T &error(Args&&... args) {
+        if (level_ <= levels::error)
+            return static_cast<T*>(this)->log(std::forward<Args>(args)...);
+        else return *static_cast<T*>(this);
+    }
+
+    template<typename... Args>
+    T &fatal(Args&&... args) {
+        if (level_ <= levels::fatal)
+            return static_cast<T*>(this)->log(std::forward<Args>(args)...);
+        else return *static_cast<T*>(this);
+    }
+protected:
+    levels level_ = levels::warn;
+
+    virtual ~level_policy() { }
+};
+
+template <bool enable, typename lock_policy, template <typename logger> typename level_policy>
+class logger_impl;
+
+template <typename lock_policy, template <typename logger> typename level_policy>
+class logger_impl<true, lock_policy, level_policy> final :
+public lock_policy,
+public level_policy<logger_impl<true, lock_policy, level_policy>>
+{
+public:
+    using base_type = logger_impl<true, lock_policy, level_policy>;
+
+    bool enable(bool status) {
+        enabled_ = status;
+        return enabled_;
+    }
+
+    bool is_enabled() const { return enabled_; }
+
+    template<typename... Args>
+    base_type &log(Args&&... args) {
         lock_policy().lock();
         if (enabled_) {
-            ((std::cout << ' ' << (eval(args))), ...);
+            ((std::cout << (eval(args)) << ' '), ...);
             std::cout << '\n';
         }
         lock_policy().unlock();
-    }
 
-    template<typename... Args>
-    void debug(Args&&... args) {
-        log(std::forward<Args>(args)...);
+        return *this;
     }
-
-    template<typename... Args>
-    void info(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void warn(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void error(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void fatal(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-private:
-    bool enabled_ = true;
+private: bool enabled_ = true;
 
     template<typename T>
     static auto eval(const T& arg) -> std::enable_if_t<!std::is_invocable_v<T>, const T&> {
@@ -77,38 +122,20 @@ private:
     }
 };
 
-template <>
-class logger_impl<false, no_lock_policy> {
+template <template <typename T> typename level_policy>
+class logger_impl<false, no_lock_policy, level_policy> final :
+public no_lock_policy,
+public level_policy<logger_impl<false, no_lock_policy, level_policy>>
+{
 public:
+    using base_type = logger_impl<true, no_lock_policy, level_policy>;
+
     bool enable(bool) { return false; }
+    bool is_enabled() const { return false; }
 
+    // FIXME: add attribute to specify that it should be ignored
     template<typename... Args>
-    void log(Args&&... args) { }
-
-    template<typename... Args>
-    void debug(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void info(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void warn(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void error(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void fatal(Args&&... args) {
-        log(std::forward<Args>(args)...);
-    }
+    logger_impl<false, no_lock_policy, level_policy> &log([[gnu::unused]] Args&&... args) { return *this; }
 private:
     template<typename T>
     static auto eval(const T& arg) -> std::enable_if_t<!std::is_invocable_v<T>, const T&> {
@@ -155,17 +182,18 @@ struct lazy_eval_impl : const_lazy_eval_impl<Args...>, non_const_lazy_eval_impl<
     { return ptr; }
 };
 
-template <typename... Args> constexpr lazy_eval_impl<Args...> lazy_eval = {};
-template <typename... Args> constexpr const_lazy_eval_impl<Args...> const_lazy_eval = {};
-template <typename... Args> constexpr non_const_lazy_eval_impl<Args...> non_const_lazy_eval = {};
+} // namespace details
+
+template <typename... Args> constexpr details::lazy_eval_impl<Args...> lazy_eval = {};
+template <typename... Args> constexpr details::const_lazy_eval_impl<Args...> const_lazy_eval = {};
+template <typename... Args> constexpr details::non_const_lazy_eval_impl<Args...> non_const_lazy_eval = {};
 
 #ifndef ENABLE_LOG
 #define ENABLE_LOG false
-#else
-#define ENABLE_LOG true
 #endif // ENABLE_LOG
 
-inline logger_impl<ENABLE_LOG, no_lock_policy> logger;
+using logging = details::logger_impl<ENABLE_LOG, details::no_lock_policy, details::level_policy>;
+inline logging logger;
 
 } // namespace utils
 
