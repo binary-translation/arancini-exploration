8ab58ce23643a79ae3e5d5c2508672f2020ae173
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2024-02-19 13:21:07 +0100

    Remove packed struct

--- inc/arancini/runtime/exec/x86/x86-cpu-state.h
+++ inc/arancini/runtime/exec/x86/x86-cpu-state.h
@@ -17,7 +17,7 @@ namespace arancini::runtime::exec::x86 {
 #define DEFREG(ctype, ltype, name) ctype name;
 #include <arancini/input/x86/reg.def>
 #undef DEFREG
-    } __attribute__((packed));
+    };
 
 #define X86_OFFSET_OF(reg) __builtin_offsetof(struct arancini::runtime::exec::x86::x86_cpu_state, reg)
 } // namespace arancini::runtime::exec::x86
