97da34fca37a277b81de11be00e3a03c113c7b95
ReimersS <sebastian.reimers@tum.de>
2024-03-03 09:12:48 +0000

    [runtime][backend-llvm] Add generic timestamps

--- inc/arancini/runtime/exec/execution-thread.h
+++ inc/arancini/runtime/exec/execution-thread.h
@@ -15,15 +15,11 @@ public:
 	~execution_thread();
 
 	void *get_cpu_state() const { return cpu_state_; }
-	std::pair<struct timespec, struct timespec> get_ts() { return {entry_, exit_}; };
 
-	void clk_entry() {
-		clock_gettime(CLOCK_THREAD_CPUTIME_ID, &entry_);
-		guest_time += ((exit_.tv_sec+exit_.tv_nsec/1000000000.0) - (entry_.tv_sec+entry_.tv_nsec/1000000000.0));
-	}
-	void clk_exit() {
-		clock_gettime(CLOCK_THREAD_CPUTIME_ID, &exit_);
-		run_time += ((entry_.tv_sec+entry_.tv_nsec/1000000000.0) - (exit_.tv_sec+exit_.tv_nsec/1000000000.0));
+	void clk(char *s) {
+		struct timespec ts;
+		clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+		std::cerr << std::dec << s << ":\t" << ts.tv_sec*1000000000+ts.tv_nsec << std::endl;
 	}
 
 	uint64_t chain_address_;
@@ -32,10 +28,5 @@ private:
 	execution_context &owner_;
 	void *cpu_state_;
 	size_t cpu_state_size_;
-	struct timespec entry_;
-	struct timespec exit_;
-
-	long double run_time;
-	long double guest_time;
 };
 } // namespace arancini::runtime::exec
