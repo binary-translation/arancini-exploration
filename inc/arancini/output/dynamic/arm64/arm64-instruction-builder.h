0a81a50b0f675b67631b0525bff070198f26e2b5
Theofilos Augoustis <theofilos.augoustis@tum.de>
2024-03-10 22:44:30 +0000

    [ARM64] Refactor logging in ARM backend

--- inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
+++ inc/arancini/output/dynamic/arm64/arm64-instruction-builder.h
@@ -677,13 +677,60 @@ public:
 
 	void dump(std::ostream &os) const;
 
+    using instruction_stream = std::vector<instruction>;
+    instruction_stream instructions() const { return instructions_; };
+
 	size_t nr_instructions() const { return instructions_.size(); }
 private:
-	std::vector<instruction> instructions_;
+	instruction_stream instructions_;
 
 	void append(const instruction &i) { instructions_.push_back(i); }
 
     void spill();
 };
+
 } // namespace arancini::output::dynamic::arm64
+
+template <>
+struct fmt::formatter<arancini::output::dynamic::arm64::instruction_builder::instruction_stream> {
+    template <typename PCTX> constexpr format_parse_context::iterator parse(const PCTX &parse_ctx) {
+        return parse_ctx.begin();
+    }
+
+    template <typename FCTX>
+    format_context::iterator format(const arancini::output::dynamic::arm64::instruction_builder::instruction_stream &regs, FCTX &format_ctx) const {
+        // TODO: call printer for instruction
+        // return fmt::format_to(format_ctx.out(), "RAX:     \t{:#x}\n"
+        //                                         "RBX:     \t{:#x}\n"
+        //                                         "RCX:     \t{:#x}\n"
+        //                                         "RDX:     \t{:#x}\n"
+        //                                         "RSI:     \t{:#x}\n"
+        //                                         "RDI:     \t{:#x}\n"
+        //                                         "RBP:     \t{:#x}\n"
+        //                                         "RSP:     \t{:#x}\n"
+        //                                         "R8:      \t{:#x}\n"
+        //                                         "R9:      \t{:#x}\n"
+        //                                         "R10:     \t{:#x}\n"
+        //                                         "R11:     \t{:#x}\n"
+        //                                         "R12:     \t{:#x}\n"
+        //                                         "R13:     \t{:#x}\n"
+        //                                         "R14:     \t{:#x}\n"
+        //                                         "R15:     \t{:#x}\n"
+        //                                         "flag ZF: \t{:#x}\n"
+        //                                         "flag CF: \t{:#x}\n"
+        //                                         "flag OF: \t{:#x}\n"
+        //                                         "flag SF: \t{:#x}\n"
+        //                                         "flag PF: \t{:#x}\n"
+        //                                         "flag DF: \t{:#x}",
+        //                       regs.RAX, regs.RBX, regs.RCX,
+        //                       regs.RDX, regs.RSI, regs.RDI,
+        //                       regs.RBP, regs.RSP, regs.R8,
+        //                       regs.R9, regs.R10, regs.R11,
+        //                       regs.R12, regs.R13, regs.R14,
+        //                       regs.R15, regs.ZF, regs.CF,
+        //                       regs.OF, regs.SF, regs.PF,
+        //                       regs.DF);
+        //
+    }
+};
 
