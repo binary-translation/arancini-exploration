ac498c65d275d8ff3b4f79575605290fa24f7fd1
ReimersS <sebastian.reimers@tum.de>
2024-05-23 09:00:13 +0000

    [ci] Add aarch64 cross target; Remove x86 native build

--- flake.nix
+++ flake.nix
@@ -95,8 +95,7 @@
 					llvmPackages.llvm.dev
 					llvmPackages.bintools
 					llvmPackages.lld
-          keystone
-				];
+				] ++ lib.optionals ( system == "aarch64-linux" ) [ keystone ];
 				depsTargetTarget = [ gcc ];
 				configurePhase = ''
 					export FLAKE_BUILD=1
@@ -107,46 +106,46 @@
 			}
 		) {};
 	}) //
-	{
-		crossPackage =	
-	let
-		my-mbuild = 
-			native_pkgs.python3Packages.buildPythonPackage {
-				pname = "mbuild";
-				version = "2022.07.28";
+	flake-utils.lib.eachSystem [ "aarch64-linux" "riscv64-linux" ] (system:
+		let
+			my-mbuild = 
+				native_pkgs.python3Packages.buildPythonPackage {
+					pname = "mbuild";
+					version = "2022.07.28";
 
-				src = mbuild-src;
-				patches = [ ./mbuild-riscv.patch ];
-			};
-		patched-xed = build_pkgs.callPackage(
-        {stdenv, lib}:
-			stdenv.mkDerivation {
-				pname = "xed";
-				version = "2022.08.11";
+					src = mbuild-src;
+					patches = [ ./mbuild-riscv.patch ];
+				};
+			patched-xed = build_pkgs.callPackage(
+    	    {stdenv, lib}:
+				stdenv.mkDerivation {
+					pname = "xed";
+					version = "2022.08.11";
 
-				src = xed-src;
-				nativeBuildInputs = [ my-mbuild ];
+					src = xed-src;
+					nativeBuildInputs = [ my-mbuild ];
 
-				buildPhase = ''
-				    patchShebangs mfile.py
+					buildPhase = ''
+					    patchShebangs mfile.py
 
-					# this will build, test and install
-				    ./mfile.py --prefix $out --toolchain riscv64-unknown-linux-gnu- --host-cpu riscv64'';
+						# this will build, test and install
+					    ./mfile.py --prefix $out'' + lib.optionalString (system == "riscv64-linux") "--toolchain riscv64-unknown-linux-gnu- --host-cpu riscv64" + lib.optionalString (system == "aarch64-linux") "--toolchain aarch64-unknown-linux-gnu- --host-cpu aarch64";
 
-				dontInstall = true; # already installed during buildPhase
-			}){};
-		fadec = build_pkgs.callPackage(
-           {stdenv, meson, ninja}:
-			stdenv.mkDerivation {
-				name = "fadec";
-				src = fadec-src;
-				nativeBuildInputs = [ meson ninja ];
-			}){};
-		native_pkgs = import nixpkgs { system = "x86_64-linux"; };
-		build_pkgs = import nixpkgs { system = "x86_64-linux"; crossSystem.config = "riscv64-unknown-linux-gnu"; };
-	in
-		build_pkgs.callPackage(
-		{stdenv, graphviz, gdb, python3, valgrind, git, cmake, pkg-config, clang, zlib, boost, libffi, libxml2, llvmPackages, lib, gcc, fmt, pkgsCross, m4}:
+					dontInstall = true; # already installed during buildPhase
+				}){};
+			fadec = build_pkgs.callPackage(
+    	       {stdenv, meson, ninja}:
+				stdenv.mkDerivation {
+					name = "fadec";
+					src = fadec-src;
+					nativeBuildInputs = [ meson ninja ];
+				}){};
+			native_pkgs = import nixpkgs { system = "x86_64-linux"; };
+			build_pkgs = import nixpkgs { system = "x86_64-linux"; crossSystem.config = system; };
+		in
+		{
+		crossPackage = build_pkgs.callPackage(
+		{stdenv, graphviz, gdb, python3, valgrind, git, cmake, pkg-config, clang, zlib, boost, libffi, libxml2, llvmPackages, lib, gcc, fmt, pkgsCross, m4, keystone}:
 			stdenv.mkDerivation {
 				name = "arancini";
 				pname = "txlat";
@@ -174,15 +173,15 @@
 					llvmPackages.llvm.dev
 					llvmPackages.bintools
 					llvmPackages.lld
-				];
+				] ++ lib.optionals ( system == "aarch64-linux" ) [ keystone ];
 				depsTargetTarget = [ gcc ];
 				configurePhase = ''
 					export FLAKE_BUILD=1
 					export NDEBUG=1
 					cmakeConfigurePhase
 				'';
-				cmakeFlags = [ "-DBUILD_TESTS=1" "-DCMAKE_BUILD_TYPE=Release" "--toolchain riscv64-toolchain-nix.cmake"];
+				cmakeFlags = [ "-DBUILD_TESTS=1" "-DCMAKE_BUILD_TYPE=Release" ] ++ lib.optionals (system == "riscv64-linux") ["--toolchain riscv64-toolchain-nix.cmake"] ++ lib.optionals (system == "aarch64-linux") ["--toolchain aarch64-toolchain-nix.cmake"];
 			}
 		) {};
-	};
+	});
 }
