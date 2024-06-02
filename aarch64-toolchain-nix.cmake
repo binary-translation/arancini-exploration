281239ab48f2ae338601928dd7f984763c4ac8b0
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2024-06-02 19:05:21 +0200

    [ARM64] Specify correct triple for cross-compilation toolchain in CMake

--- aarch64-toolchain-nix.cmake
+++ aarch64-toolchain-nix.cmake
@@ -4,7 +4,7 @@
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR AARCH64)
-set(TOOLCHAIN_PREFIX aarch64-unknown-linux-gnu)
+set(TOOLCHAIN_PREFIX aarch64-linux-gnu)
 
 # Set DBT architecture
 set(DBT_ARCH AARCH64)
