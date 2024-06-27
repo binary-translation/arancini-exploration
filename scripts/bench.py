2880dbbfec7d7e1eb94e9c6f548fe35c5e8e08a8
ReimersS <sebastian.reimers@tum.de>
2024-06-27 13:19:14 +0000

    [bench] Refactor bench.py

--- scripts/bench.py
+++ scripts/bench.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import os
 import argparse
 import configparser
+import itertools as it
 
 def main():
     parser = argparse.ArgumentParser(description='Run benchmarks')
@@ -48,15 +49,12 @@ def setup(linkname):
 def build():
     pass
 
-def do_run(progs, argf, name, env, config):
+def do_run(progs, env):
     print(f"Running {progs} ...")
-    args = ""
-    if argf != "":
-        args = config['x86']['PHOENIX_X86_PATH']+"/"+name+"_datafiles/"+argf
     start = datetime.now()
     env["ARANCINI_ENABLE_LOG"] = "false";
     try:
-        out = sp.run(progs + [args], capture_output=True, env=env, timeout=1800)
+        out = sp.run(["sudo", "nice", "-n", "-25"] + progs, capture_output=True, env=env, timeout=1800)
     except:
         return -1
     end = datetime.now()
@@ -66,95 +64,72 @@ def do_run(progs, argf, name, env, config):
         return -1
     return dif/timedelta(microseconds=1)
 
-ty = { "":"map-reduce", "-seq":"sequential", "-pthread":"pthreads" }
+def to_datafile(pre, name, ver):
+    return f"{pre}/{name}_datafiles/{ver}"
+
 def run(csvfile, config): 
-    progs = {"histogram":"small.bmp", "kmeans":"", "pca":"", "string_match":"key_file_50MB.txt", "matrix_multiply":""}
     fieldnames = [ "benchmark", "emulator", "time", "type", "threads"]
     writer = csv.DictWriter(csvfile, fieldnames)
 
     PHOENIX_DIR_PATH = config['x86']['PHOENIX_X86_PATH']
     ARANCINI_RESULT_PATH = config['arancini']['ARANCINI_RESULT_PATH'];
-    ARANCINI_OUT_PATH = config['translations']['ARANCINI_OUT_PATH'];
-    MCTOLL_OUT_PATH = config['translations']['MCTOLL_OUT_PATH'];
-    QEMU_PATH = "qemu-x86_64";
-    RISOTTO_PATH = "risotto";
+    
+    # literaly too lazy to type
+    p = PHOENIX_DIR_PATH
+    progs = {
+        "histogram":to_datafile(p, "histogram", "small.bmp"),
+        "kmeans":"",
+        "pca":"",
+        "string_match":to_datafile(p, "string_match", "key_file_50MB.txt"),
+        "matrix_multiply":"1024 1024",
+        "word_count":to_datafile(p, "word_count", "word_10MB.txt"),
+        "linear_regression":to_datafile(p, "linear_regression", "key_file_50MB.txt")
+    }
+
+    native = list(map(lambda l: (l,[ f'{config["native"]["PHOENIX_DIR_PATH"]}{l}', progs[l] ]), progs.keys()))
+
+    arancini = list(map(lambda l: (l,[ f'{config["translations"]["ARANCINI_OUT_PATH"]}{l}.out', progs[l] ]), progs.keys()))
+    arancini_dyn = list(map(lambda l: (l,[ f'{config["translations"]["ARANCINI_OUT_PATH"]}{l}-dyn.out', progs[l] ]), progs.keys()))
+    arancini_nmem = list(map(lambda l: (l,[ f'{config["translations"]["ARANCINI_OUT_PATH"]}{l}-nmem.out', progs[l] ]), progs.keys()))
+    arancini_nlock = list(map(lambda l: (l,[ f'{config["translations"]["ARANCINI_OUT_PATH"]}{l}-nlock.out', progs[l] ]), progs.keys()))
+
+    risotto = list(map(lambda l: (l,[ 'risotto', f'{PHOENIX_DIR_PATH}{l}', progs[l] ]), progs.keys()))
+    risotto_qemu = list(map(lambda l: (l,[ 'risotto-qemu', f'{PHOENIX_DIR_PATH}{l}', progs[l] ]), progs.keys()))
+    risotto_nofence = list(map(lambda l: (l,[ 'risotto-nofence', f'{PHOENIX_DIR_PATH}{l}', progs[l] ]), progs.keys()))
+    risotto_nmem = list(map(lambda l: (l,[ 'risotto', '-nlib', 'general.mni', f'{PHOENIX_DIR_PATH}{l}', progs[l] ]), progs.keys()))
+    risotto_nlock = list(map(lambda l: (l,[ 'risotto', '-nlib', 'lock.mni', f'{PHOENIX_DIR_PATH}{l}', progs[l] ]), progs.keys()))
+
+    threads = [ 2, 4, 8 ]
+
+    runs = {
+            "native":native,
+            "Arancini":arancini,
+            "Arancini-nmem":arancini_nmem,
+            "Arancini-nlock":arancini_nlock,
+            "Risotto":risotto,
+            "Risotto-QEMU":risotto_qemu,
+            "Risotto-nofence":risotto_nofence,
+            "Risotto-nmem":risotto_nmem,
+            "Risotto-nlock":risotto_nlock
+        }
 
     # because matrix_multiply wants to be special
     prog = [config["native"]["PHOENIX_DIR_PATH"]+"matrix_multiply"]
     sp.run(prog + ["1024 1024 1"], capture_output=True, timeout=1800) 
 
-    for p in progs.keys():
-        f = progs[p]
-        for suffix in [""]: #, "-seq", "-pthread"]:
-            th = [1]
-            if suffix != "-seq":
-                th = [2, 4, 8];
-            
-            for threads in th:
-                for i in range(3):
-                    #native
-                    env = os.environ
-                    prog = ["taskset", "-c", f"1-{threads}", config["native"]["PHOENIX_DIR_PATH"]+p+suffix]
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"native", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-
-                    #txlat
-                    env["LD_LIBRARY_PATH"] = ARANCINI_RESULT_PATH+"lib"
-                    prog = ["taskset", "-c", f"1-{threads}", ARANCINI_OUT_PATH+p+suffix+".out"]
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Arancini", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-
-                    #txlat-dyn
-                    env["LD_LIBRARY_PATH"] = ARANCINI_RESULT_PATH+"lib"
-                    prog = ["taskset", "-c", f"1-{threads}", ARANCINI_OUT_PATH+p+suffix+"-dyn.out"]
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Arancini-Dyn", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-
-                    #txlat-nlib
-                    env["LD_LIBRARY_PATH"] = ARANCINI_RESULT_PATH+"lib"
-                    prog = ["taskset", "-c", f"1-{threads}", ARANCINI_OUT_PATH+p+suffix+"-nmem.out"]
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Arancini-Native-Mem", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-
-                    #QEMU
-                    env["LD_LIBRARY_PATH"] = PHOENIX_DIR_PATH
-                    prog = ["taskset", "-c", f"1-{threads}", QEMU_PATH, PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"QEMU", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    
-                    prog = ["taskset", "-c", f"1-{threads}", RISOTTO_PATH, PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Risotto", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    prog = ["taskset", "-c", f"1-{threads}", RISOTTO_PATH+"-qemu", PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Risotto-qemu", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    prog = ["taskset", "-c", f"1-{threads}", RISOTTO_PATH+"-nofence", PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Risotto-nofence", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    prog = ["taskset", "-c", f"1-{threads}", RISOTTO_PATH+"-tso", PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Risotto-tso", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    prog = ["taskset", "-c", f"1-{threads}", RISOTTO_PATH, "-nlib", "general.mni", PHOENIX_DIR_PATH+p+suffix] 
-                    dif = do_run(prog, f, p, env, config)
-                    writer.writerow({"benchmark":p, "emulator":"Risotto-Native-Mem", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    csvfile.flush()
-                    #MCtoll
-                    #prog = ["taskset", "-c", f"1-{threads}", MCTOLL_OUT_PATH+p+suffix+".ll.out"] 
-                    #dif = do_run(prog, f, p, env)
-                    #writer.writerow({"benchmark":p, "emulator":"mctoll", "time":str(dif), "type":ty[suffix], "threads":f"{threads}"})
-                    #csvfile.flush()
+    for t in threads:
+        for r in runs.keys():
+            for b, run in runs[r]:
+                env = os.environ
+                env["LD_LIBRARY_PATH"] = ARANCINI_RESULT_PATH+"lib"
+                print(f'### {run}')
+                prog = ["taskset", "-c", f"1-{t}"] + run
+                dif = do_run(prog, env)
+                writer.writerow({"benchmark":b, "emulator":r, "time":str(dif), "type":"map-reduce", "threads":f"{t}"})
+                csvfile.flush()
 
 def clean(csvfile):
-    print("Cleaning ...")
-    close(csvfile)
+    pass
 
 if __name__ == "__main__":
     main()
