260d6dd3905e9ce1173e7e5c6ab97b501ca20405
ReimersS <sebastian.reimers@tum.de>
2024-06-27 13:16:58 +0000

    [backend-llvm] Fixup fence-combine pass

--- src/output/static/llvm/llvm-fence-combine.cpp
+++ src/output/static/llvm/llvm-fence-combine.cpp
@@ -1,6 +1,7 @@
 #include "arancini/output/static/llvm/llvm-fence-combine.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
+#include <llvm/Support/AtomicOrdering.h>
 
 using namespace ::llvm;
 
@@ -9,39 +10,28 @@ PreservedAnalyses FenceCombinePass::run(Function &F, FunctionAnalysisManager &AM
 	for (BasicBlock &BB : F) {
 
 		auto It = BB.begin();
+		IRBuilder<> builder(F.getContext());
 
+		FenceInst *first = nullptr;
 		while (It != BB.end()) {
 			Instruction &I = *It;
-			It++;
 
-			// Combines two consecutive fence instructions into a single one.
 			if (auto *Fence1 = dyn_cast<FenceInst>(&I)) {
-				if (It == BB.end())
-					break;
-				Instruction &NextI = *It;
-				if (auto *Fence2 = dyn_cast<FenceInst>(&NextI)) {
-					auto ord1 = Fence1->getOrdering();
-					auto ord2 = Fence1->getOrdering();
-					// combine orderings, ignore consume because no one implements it
-					// most common case is aquire and release to aquire-release
-					if (ord1 == AtomicOrdering::Acquire && ord2 == AtomicOrdering::Release) {
-						Fence1->setOrdering(AtomicOrdering::AcquireRelease);
-					}
-					else if (ord1 == AtomicOrdering::Release && ord2 == AtomicOrdering::Acquire) {
-						Fence1->setOrdering(AtomicOrdering::AcquireRelease);
-					}
-					else if (ord1 == AtomicOrdering::SequentiallyConsistent || ord2 == AtomicOrdering::SequentiallyConsistent) {
-						Fence1->setOrdering(AtomicOrdering::SequentiallyConsistent);
-					}
-					else if (ord1 == AtomicOrdering::AcquireRelease || ord2 == AtomicOrdering::AcquireRelease) {
-						Fence1->setOrdering(AtomicOrdering::AcquireRelease);
-					}
-
-					// assume the scopes are the same
-					Fence2->eraseFromParent();
+				if (first) {
 					It++;
+					// combine the two fences
+					first->setOrdering(getMergedAtomicOrdering(first->getOrdering(), Fence1->getOrdering()));
+					Fence1->eraseFromParent();
+					first = nullptr;
+					continue;
+				} else {
+					first = Fence1;
 				}
+			} else {
+				if (It->mayReadOrWriteMemory())
+					first = nullptr;
 			}
+			It++;
 		}
 	}
 	// probably true?
