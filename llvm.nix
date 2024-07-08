89712515228712aa63f9963d985af580b342270e
Martin Fink <martin@finkmartin.com>
2024-07-08 15:46:41 +0000

    Update llvm

--- llvm.nix
+++ llvm.nix
@@ -4,12 +4,12 @@ pkgs.llvmPackages_16.override ({
   monorepoSrc = pkgs.fetchFromGitHub {
     owner = "martin-fink";
     repo = "llvm-project";
-    rev = "568e4ae228fc2f5f9c9d026ca328c59c98a63026";
-    sha256 = "sha256-3ZCoog5Nwm1bBH9HBraa9c7yzhcSVM3C5Y9MKrPQS38=";
+    rev = "ed84a56b98fe3de443d47a71630901dad01ee820";
+    sha256 = "sha256-PiwfcZP+9K3TDUoMnoTgugEuw2t7sUiqYlg+H6yyF68=";
   };
   gitRelease = {
     version = "16.0.6";
-    rev = "568e4ae228fc2f5f9c9d026ca328c59c98a63026";
+#    rev = "568e4ae228fc2f5f9c9d026ca328c59c98a63026";
     rev-version = "16.0.6-arancini";
   };
   officialRelease = null;
