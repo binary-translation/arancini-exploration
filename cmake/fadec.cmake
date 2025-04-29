74389f6393a6957897fc14961bca80d4d4dffd36
Nathan Mossaad <git@nathanmossaad.com>
2025-04-29 18:07:18 +0000

    Fix: Cleanup cmake for fadec

--- cmake/fadec.cmake
+++ cmake/fadec.cmake
@@ -18,18 +18,21 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(fadec)
 
+# Specify only the files that your command generates.
 add_custom_command(
-  OUTPUT ${fadec_SOURCE_DIR}/ ${fadec_SOURCE_DIR}/fadec-encode-private.inc
+  OUTPUT ${fadec_SOURCE_DIR}/fadec-encode-public.inc
+         ${fadec_SOURCE_DIR}/fadec-encode-private.inc
   COMMAND
     ${Python3_EXECUTABLE} ${fadec_SOURCE_DIR}/parseinstrs.py encode
     ${fadec_SOURCE_DIR}/instrs.txt ${fadec_SOURCE_DIR}/fadec-encode-public.inc
-    ${fadec_SOURCE_DIR}/fadec-encode-private.inc)
+    ${fadec_SOURCE_DIR}/fadec-encode-private.inc
+  DEPENDS ${fadec_SOURCE_DIR}/parseinstrs.py ${fadec_SOURCE_DIR}/instrs.txt
+  COMMENT "Generating fadec encoding header files")
 
 add_custom_target(
   fadec-defs ALL DEPENDS ${fadec_SOURCE_DIR}/fadec-encode-public.inc
                          ${fadec_SOURCE_DIR}/fadec-encode-private.inc)
 
 add_library(fadec ${fadec_SOURCE_DIR}/encode.c)
-
 target_include_directories(fadec PUBLIC ${fadec_SOURCE_DIR})
 add_dependencies(fadec fadec-defs)
