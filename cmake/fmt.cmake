cf4769fdd3594f027aaa847086608327a91e5bc5
Nathan Mossaad <git@nathanmossaad.com>
2025-04-29 09:32:54 +0000

    Cleanup cmake for fmt and getKeystone
    We need to force specific git tags, to ensure that the correct (old) versions are used outside of a nix environment.

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,31 +1,16 @@
 cmake_minimum_required(VERSION 3.22)
 project(fmt)
 
+include(FetchContent)
+
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-set(FMT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/fmt)
+# We want to use the same fmt version as NixOS (as of 2024-04-29)
+FetchContent_Declare(
+    fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+  GIT_TAG 10.2.1)
 
-if(NOT EXISTS ${FMT_DIR})
-  find_package(Git QUIET)
-  if(Git_FOUND)
-    # Fetch FMT from repo
-    message(STATUS "Git found: fetching release via git")
+FetchContent_MakeAvailable(fmt)
 
-    FetchContent_Declare(
-      fmt
-      GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-      SOURCE_DIR ${FMT_DIR})
-  else()
-    message(STATUS "Git not found: fetching release via archive")
-
-    FetchContent_Declare(
-      fmt URL https://github.com/fmtlib/fmt/archive/refs/heads/master.zip
-              DOWNLOAD_DIR ${FMT_DIR}/.. SOURCE_DIR ${FMT_DIR})
-  endif()
-
-  # Populate FMT directory
-  FetchContent_Populate(fmt)
-endif()
-
-add_subdirectory(${FMT_DIR})
-include_directories(${FMT_DIR}/include)
+include_directories(${fmt_SOURCE_DIR}/include)
---
+++ src/output/dynamic/arm64/cmake/getKeystone.cmake
@@ -0,0 +1,36 @@
+cmake_minimum_required(VERSION 3.22)
+project(Keystone)
+
+message("Downloading Keystone")
+
+
+include(FetchContent)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+# We want to use the same keystone version as NixOS (as of 2024-04-29)
+FetchContent_Declare(
+  keystone
+  GIT_REPOSITORY https://github.com/keystone-engine/keystone.git
+  GIT_TAG 0.9.2)
+
+
+FetchContent_MakeAvailable(keystone)
+
+message("keystone_SOURCE_DIR = ${keystone_SOURCE_DIR}")
+
+# Apply patch
+execute_process(
+  COMMAND patch -p1 -i ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/keystone.patch
+  WORKING_DIRECTORY ${keystone_SOURCE_DIR}
+  RESULT_VARIABLE patch_result
+)
+if(NOT patch_result EQUAL 0)
+    message(FATAL_ERROR "Patching Keystone failed with error code: ${patch_result}")
+endif()
+
+
+set(BUILD_LIBS_ONLY
+    ON
+    CACHE BOOL "Disable test build" FORCE)
+include_directories(${keystone_SOURCE_DIR}/include/keystone)
