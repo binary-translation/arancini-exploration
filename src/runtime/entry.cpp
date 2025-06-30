a5e2d76ceb11cd7387fce97b11843006ec5607e5
Nathan Mossaad <git@nathanmossaad.com>
2025-06-30 16:23:04 +0000

    Fix: X87_CTRL not initialized correctly

--- src/runtime/entry.cpp
+++ src/runtime/entry.cpp
@@ -308,6 +308,7 @@ extern "C" void *initialise_dynamic_runtime(unsigned long entry_point, int argc,
                        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) -
         (intptr_t)ctx_->get_memory_ptr(0);
     x86_state->X87_TAG = std::uint16_t(0xFFFF);
+    x86_state->X87_CTRL = std::uint16_t(0x037F);
 
     // Report on various information for useful debugging purposes.
     util::global_logger.info("state={} pc={:#x} stack={:#x}\n",
