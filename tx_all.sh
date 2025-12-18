1d1693979881d9de8182debcb16789222e0df5ca
ReimersS <sebastian.reimers@tum.de>
2025-12-18 13:41:19 +0000

    fixup! Fixup wrong number of bars in plots

--- tx_all.sh
+++ tx_all.sh
@@ -7,7 +7,7 @@ tx_all() {
   echo "Translating all Benchmarks"
   for b in test/phoenix/*; do
     if [[ $(basename $b) != *.so ]]; then
-      result-aarch64/bin/txlat -I $b -O txlat-aarch64/$(basename $b) -l txlat-aarch64/libc.so -l txlat-aarch64/libunwind.so
+      result/bin/txlat -I $b -O txlat-aarch64/$(basename $b) -l txlat-aarch64/libc.so -l txlat-aarch64/libunwind.so
     fi
   done
 }
@@ -17,7 +17,7 @@ tx_all_ndf() {
   echo "Translating all Benchmarks without deadflag opt"
   for b in test/phoenix/*; do
     if [[ $(basename $b) != *.so ]]; then
-      result-aarch64/bin/txlat --disable-flag-opt -I $b -O txlat-nodeadflags-aarch64/$(basename $b) -l txlat-nodeadflags-aarch64/libc.so -l txlat-nodeadflags-aarch64/libunwind.so
+      result/bin/txlat --disable-flag-opt -I $b -O txlat-nodeadflags-aarch64/$(basename $b) -l txlat-nodeadflags-aarch64/libc.so -l txlat-nodeadflags-aarch64/libunwind.so
     fi
   done
 }
@@ -27,7 +27,7 @@ tx_all_nfm() {
   echo "Translating all Benchmarks without fencemerge opt"
   for b in test/phoenix/*; do
     if [[ $(basename $b) != *.so ]]; then
-      result-aarch64/bin/txlat --disable-flag-opt -I $b -O txlat-nofencemerge-aarch64/$(basename $b) -l txlat-nofencemerge-aarch64/libc.so -l txlat-nofencemerge-aarch64/libunwind.so
+      result/bin/txlat --disable-flag-opt -I $b -O txlat-nofencemerge-aarch64/$(basename $b) -l txlat-nofencemerge-aarch64/libc.so -l txlat-nofencemerge-aarch64/libunwind.so
     fi
   done
 }
@@ -37,7 +37,7 @@ tx_all_fast() {
   echo "Translating all Benchmarks with native libs"
   for b in test/phoenix/*; do
     if [[ $(basename $b) != *.so ]]; then
-      result-aarch64/bin/txlat --nlib general.mni.aarch64.long -I $b -O txlat-fast-aarch64/$(basename $b) -l txlat-fast-aarch64/libc.so -l txlat-fast-aarch64/libunwind.so
+      result/bin/txlat --nlib general.mni.aarch64.long -I $b -O txlat-fast-aarch64/$(basename $b) -l txlat-fast-aarch64/libc.so -l txlat-fast-aarch64/libunwind.so
     fi
   done
 }
